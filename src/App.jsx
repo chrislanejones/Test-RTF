57f52d4a03f422f3085b5de78a71dba3e5a764a0
Chris Lane Jones <chrislanejones@gmail.com>
2023-11-28 06:39:00 -0500

    Cube Moves With Mouse

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
 function App() {
   return (
     <Canvas camera={{ position: [3, 3, 3] }}>
+      <OrbitControls />
       <mesh>
         <boxGeometry />
         <meshNormalMaterial />

a286bd3e596d49c8b2bdf6eb072172788e0b06a7
Chris Lane Jones <chrislanejones@gmail.com>
2023-11-29 01:50:09 -0500

    Two Squares

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
+import * as THREE from "three";
+
 function App() {
   return (
-    <Canvas camera={{ position: [0, 3, 3] }}>
+    <Canvas camera={{ position: [3, 3, 3] }}>
       <OrbitControls />
-      <mesh>
-        <cylinderGeometry args={[2, 3, 3, 32]} />
-        <meshNormalMaterial />
+      <mesh position-x={-0.6}>
+        <boxGeometry />
+        <meshStandardMaterial color="hotpink" side={THREE.FrontSide} />
       </mesh>
+      <mesh position-x={0.6} position-z={-1}>
+        <boxGeometry />
+        <meshStandardMaterial color="skyblue" side={THREE.BackSide} />
+      </mesh>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 0, 3]} intensity={5} />
+      <directionalLight position={[0, 3, 3]} intensity={1} />
+      <directionalLight position={[3, 3, 3]} intensity={1} />
     </Canvas>
   );
 }

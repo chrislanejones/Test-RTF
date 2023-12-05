67ddf117b23fc336635396ef3d8921a1712f0b24
Chris Lane Jones <chrislanejones@gmail.com>
2023-12-05 03:34:05 -0500

    Moved the cool shadows design to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,75 @@
-import { OrbitControls, Grid, Stats } from "@react-three/drei";
+import {
+  AccumulativeShadows,
+  ContactShadows,
+  OrbitControls,
+  RandomizedLight,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import * as THREE from "three";
-
+import { useControls } from "leva";
 function App() {
-  return (
-    <Canvas camera={{ position: [0, 3, 8] }}>
-      <Stats />
-      {/* This Grid adds a field for debugging */}
-      <Grid
-        sectionSize={3}
-        sectionColor={"purple"}
-        sectionThickness={1}
-        cellSize={1}
-        cellColor={"#6f6f6f"}
-        cellThickness={0.6}
-        infiniteGrid
-        fadeDistance={50}
-        fadeStrength={5}
-      />
-      <OrbitControls />
-      {/* Lighting */}
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 3, 5]} intensity={0.5} />
+  const { cubeInAir } = useControls({
+    cubeInAir: false,
+  });
 
-      {/* Objects */}
-      <group position={[-2, -2, 0]} scale={[2, 2, 2]} rotation-y={Math.PI / 4}>
-        <mesh position-x={-0.6}>
-          <boxGeometry />
-          <meshStandardMaterial color="hotpink" side={THREE.FrontSide} />
-        </mesh>
-        <mesh position-x={0.6} position-z={-1}>
-          <boxGeometry />
-          <meshStandardMaterial color="skyblue" side={THREE.BackSide} />
-        </mesh>
-        <mesh position-x={0.6} position-z={-1}>
-          <torusGeometry args={[6, 2, 5, 40]} />
-          <meshPhongMaterial attach="material" color={"green"} />
-        </mesh>
+  return (
+    <>
+      <Canvas shadows camera={{ position: [0, 3, 3] }}>
+        <OrbitControls />
+        <AccumulativeShadows
+          temporal
+          frames={35}
+          alphaTest={0.85}
+          scale={5}
+          position={[0, -0.499, 0]}
+          color="#EFBD4E"
+        >
+          <RandomizedLight
+            amount={4}
+            radius={9}
+            intensity={0.55}
+            ambient={0.25}
+            position={[5, 5, -10]}
+          />
+          <RandomizedLight
+            amount={4}
+            radius={5}
+            intensity={0.25}
+            ambient={0.55}
+            position={[-5, 5, -9]}
+          />
+        </AccumulativeShadows>
+        <ContactShadows
+          position-y={-0.49}
+          opacity={0.5}
+          blur={2}
+          color={"pink"}
+          scale={10}
+        />
         <ambientLight intensity={0.5} />
-        <directionalLight position={[0, 0, 3]} intensity={5} />
-        <directionalLight position={[0, 3, 3]} intensity={1} />
-        <directionalLight position={[3, 3, 3]} intensity={1} />
-      </group>
-    </Canvas>
+        <directionalLight position={[5, 5, 5]} intensity={0.5} />
+        <directionalLight position={[-5, 5, 5]} intensity={0.5} color="red" />
+
+        <mesh position={[1, 1, 1]} castShadow>
+          <sphereGeometry args={[0.5, 32, 32]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
+
+        <mesh
+          rotation-y={Math.PI / 4}
+          castShadow
+          receiveShadow
+          position-y={cubeInAir ? 1 : 0}
+        >
+          <boxGeometry />
+          <meshStandardMaterial color="white" />
+        </mesh>
+
+        <mesh rotation-x={-Math.PI / 2} position-y={-0.5} receiveShadow>
+          <planeGeometry args={[5, 5]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
+      </Canvas>
+    </>
   );
 }
 

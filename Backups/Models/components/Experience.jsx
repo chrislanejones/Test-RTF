55d212b72f40104644bcee6dc6557ab4a0aaeaa3
Chris Lane Jones <chrislanejones@gmail.com>
2023-12-09 00:00:53 -0500

    Changing it and FBX style file

--- Backups/Models/components/Experience.jsx
+++ Backups/Models/components/Experience.jsx
@@ -1,11 +1,13 @@
-import { useGLTF } from "@react-three/drei";
+import { useFBX, useGLTF } from "@react-three/drei";
 
 export const Experience = () => {
   const { scene } = useGLTF("models/Fish.gltf");
-
+  const dino = useFBX("models/Dino.fbx");
   return (
     <>
       <primitive object={scene} />
+      <primitive object={dino} />
     </>
   );
 };
+

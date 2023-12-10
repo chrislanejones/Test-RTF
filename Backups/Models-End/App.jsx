f9a61d82d5014fb67e9095f99a2a48b32059323b
Chris Lane Jones <chrislanejones@gmail.com>
2023-12-10 02:20:40 -0500

    Fish with controls

---
+++ Backups/Models-End/App.jsx
@@ -0,0 +1,17 @@
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
+function App() {
+  return (
+    <>
+      <Canvas camera={{ position: [-1.5, 3, 10], fov: 42 }}>
+        <Experience />
+        <OrbitControls />
+        <ambientLight intensity={1} />
+        <Environment preset="sunset" />
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
---
+++ Backups/Models-End/components/Experience.jsx
@@ -0,0 +1,10 @@
+import { useFBX, useGLTF } from "@react-three/drei";
+import { Fish } from "./Fish";
+
+export const Experience = () => {
+  return (
+    <>
+      <Fish />
+    </>
+  );
+};

09faa5f42acddabb0d39d2a5eefc09743435e18a
Chris Lane Jones <chrislanejones@gmail.com>
2023-12-20 04:29:44 -0500

    ThreeJS Animations Framer Motion

--- Backups/animations/src/components/Background.jsx
+++ Backups/animations/src/components/Background.jsx
@@ -1,3 +1,4 @@
+import { useScroll } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import gsap from "gsap";
 import { useEffect, useRef } from "react";
@@ -11,20 +12,24 @@ export const Background = () => {
 
   useEffect(() => {
     tl.current = gsap.timeline();
-    tl.current.to(skyData.current, {
-      duration: 1,
-      color: "#ffc544",
-    });
-    tl.current.to(skyData.current, {
-      duration: 1,
-      color: "#7c4e9f",
-    });
+    tl.current
+      .to(skyData.current, {
+        duration: 1,
+        color: "#ffc544",
+      })
+      .to(skyData.current, {
+        duration: 1,
+        color: "#7c4e9f",
+      });
+    tl.current.pause();
   }, []);
 
+  const scrollData = useScroll();
   useFrame(() => {
     if (!tl.current) {
       return;
     }
+    tl.current.progress(scrollData.offset);
     skyMaterial.current.color.set(skyData.current.color);
   });
 
--- Backups/animations/src/components/Teeth.jsx
+++ Backups/animations/src/components/Teeth.jsx
@@ -1,18 +1,106 @@
+import { motion } from "framer-motion-3d";
+import { useEffect, useState } from "react";
+
 export const Teeth = () => {
+  const [variant, setVariant] = useState("closed");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setVariant((prev) => (prev === "opened" ? "closed" : "opened"));
+    }, 1500);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <group>
-      <mesh position-x={-1} position-y={-1}>
+    <motion.group animate={variant}>
+      <motion.mesh
+        position-x={-1}
+        position-y={-1}
+        variants={{
+          closed: {
+            y: -1,
+            rotateY: 0,
+          },
+          opened: {
+            scale: 1.1,
+            y: 0,
+            rotateY: Math.PI / 2,
+          },
+        }}
+      >
         <coneGeometry args={[0.5, 1, 4]} />
-        <meshStandardMaterial color="#ffffff" />
-      </mesh>
-      <mesh position-x={0} position-y={1} rotation-x={Math.PI}>
+        <motion.meshStandardMaterial
+          color="#ffffff"
+          variants={{
+            closed: {
+              color: "#ffffff",
+            },
+            opened: {
+              color: "#7564a4",
+            },
+          }}
+        />
+      </motion.mesh>
+      <motion.mesh
+        position-x={0}
+        position-y={1}
+        rotation-x={Math.PI}
+        variants={{
+          closed: {
+            y: 1,
+            rotateY: 0,
+          },
+          opened: {
+            scale: 1.2,
+            y: 0,
+            rotateY: Math.PI / 2,
+          },
+        }}
+      >
         <coneGeometry args={[0.5, 1, 4]} />
-        <meshStandardMaterial color="#ffffff" />
-      </mesh>
-      <mesh position-x={1} position-y={-1}>
+        <motion.meshStandardMaterial
+          color="#ffffff"
+          variants={{
+            closed: {
+              color: "#ffffff",
+            },
+            opened: {
+              color: "#7c5ecf",
+            },
+          }}
+        />
+      </motion.mesh>
+      <motion.mesh
+        position-x={1}
+        position-y={-1}
+        variants={{
+          closed: {
+            y: -1,
+            rotateY: 0,
+          },
+          opened: {
+            scale: 1.1,
+            y: 0,
+            rotateY: Math.PI / 2,
+          },
+        }}
+      >
         <coneGeometry args={[0.5, 1, 4]} />
-        <meshStandardMaterial color="#ffffff" />
-      </mesh>
-    </group>
+        <motion.meshStandardMaterial
+          color="#ffffff"
+          variants={{
+            closed: {
+              color: "#ffffff",
+            },
+            opened: {
+              color: "#6232e6",
+            },
+          }}
+        />
+      </motion.mesh>
+    </motion.group>
   );
 };

2136d2615749838ac6e4974d2122d392a5ee146d
Chris Lane Jones <chrislanejones@gmail.com>
2024-01-05 22:40:29 -0500

    Post Processing with Effect Composer

---
+++ Backups/Post-Processing/src/App.jsx
@@ -0,0 +1,16 @@
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
+import { Effects } from "./components/Effects";
+function App() {
+  return (
+    <>
+      <Canvas camera={{ position: [-1, 0.225, 5.91], fov: 42 }}>
+        <color attach="background" args={["#333333"]} />
+        <Experience />
+        <Effects />
+      </Canvas>
+    </>
+  );
+}
+
+export default App;

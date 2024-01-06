ddffdc2d4ddcbc32a61b5ab3a65da6380f9a0a8a
Chris Lane Jones <chrislanejones@gmail.com>
2024-01-06 02:10:32 -0500

    Bloom and Candle Light fixed and Brightness and Contrast added to leva

--- Backups/Post-Processing/src/components/Effects.jsx
+++ Backups/Post-Processing/src/components/Effects.jsx
@@ -1,4 +1,9 @@
-import { Bloom, EffectComposer, Vignette } from "@react-three/postprocessing";
+import {
+  Bloom,
+  BrightnessContrast,
+  EffectComposer,
+  Vignette,
+} from "@react-three/postprocessing";
 import { useControls } from "leva";
 
 export const Effects = () => {
@@ -18,13 +23,29 @@ export const Effects = () => {
   const bloomConfig = useControls("bloom", {
     enabled: true,
     luminanceThreshold: { value: 1, min: 0, max: 2 },
-    intensity: { value: 0.5, min: 0, max: 2 },
+    intensity: { value: 1.28, min: 0, max: 2 },
     mipmapBlur: true,
+  });
+  const brightnessContrastConfig = useControls("brightnessContrast", {
+    enabled: true,
+    brightness: {
+      value: 0.1,
+      min: 0,
+      max: 1,
+    },
+    contrast: {
+      value: 1.28,
+      min: 0,
+      max: 1,
+    },
   });
   return (
     <EffectComposer disableNormalPass>
       {vignetteConfig.enabled && <Vignette {...vignetteConfig} />}
       {bloomConfig.enabled && <Bloom {...bloomConfig} />}
+      {brightnessContrastConfig.enabled && (
+        <BrightnessContrast {...brightnessContrastConfig} />
+      )}
     </EffectComposer>
   );
 };

270428d2eb559423dc03f7ca8566f226c0e69e01
Chris Lane Jones <chrislanejones@gmail.com>
2024-01-05 23:55:01 -0500

    Vignette

--- Backups/Post-Processing/src/components/Effects.jsx
+++ Backups/Post-Processing/src/components/Effects.jsx
@@ -1,5 +1,23 @@
-import { EffectComposer } from "@react-three/postprocessing";
+import { EffectComposer, Vignette } from "@react-three/postprocessing";
+import { useControls } from "leva";
 
 export const Effects = () => {
-  return <EffectComposer disableNormalPass></EffectComposer>;
+  const vignetteConfig = useControls("vignette", {
+    enabled: true,
+    offset: {
+      value: 0.1,
+      min: 0,
+      max: 0,
+    },
+    darkness: {
+      value: 0.92,
+      min: 0,
+      max: 0,
+    },
+  });
+  return (
+    <EffectComposer disableNormalPass>
+      {vignetteConfig.enabled && <Vignette {...vignetteConfig} />}
+    </EffectComposer>
+  );
 };

76144f719216121a4d860770b5db9ae5f2604c73
Chris Lane Jones <chrislanejones@gmail.com>
2024-01-07 04:40:23 -0500

    Ball no longer works

--- Ball-Site/main.js
+++ Ball-Site/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import "./style.css";
 
 const scene = new THREE.Scene();
 
@@ -18,11 +19,16 @@ const light = new THREE.PointLight(0xffffff, 1, 100);
 light.position.set(0, 5, 10);
 scene.add(light);
 
-const camera = new THREE.PerspectiveCamera(45, 800 / 600, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
 camera.position.z = 20;
 scene.add(camera);
 
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);

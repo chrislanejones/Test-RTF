26030aae555d4d1914564943b975b76e253b3359
Chris Lane Jones <chrislanejones@gmail.com>
2024-01-08 22:57:26 -0500

    Mouse movements I

--- Ball-Site/main.js
+++ Ball-Site/main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import "./style.css";
+import gsap from "gsap";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
@@ -56,3 +57,8 @@ const loop = () => {
   window.requestAnimationFrame(loop);
 };
 loop();
+
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+tl.fromTo("nav", { y: "100%" }, { y: "0%" });
+tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });

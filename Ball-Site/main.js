66e6a7c312643460d3eee8b948fda263af4de0dc
Chris Lane Jones <chrislanejones@gmail.com>
2024-01-08 23:03:33 -0500

    Mouse movements page detect

--- Ball-Site/main.js
+++ Ball-Site/main.js
@@ -62,3 +62,15 @@ const tl = gsap.timeline({ defaults: { duration: 1 } });
 tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
 tl.fromTo("nav", { y: "100%" }, { y: "0%" });
 tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });
+
+let mouseDown = false;
+let rgb = [];
+window.addEventListener("mousedown", () => (mouseDown = true));
+window.addEventListener("mouseup", () => (mouseDown = false));
+
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [Math.round((e.pageX / sizes.width) * 255)];
+    console.log(rgb);
+  }
+});

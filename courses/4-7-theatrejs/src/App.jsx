9fe50447bd2b891db724a67d63e5da54b411bf9d
Chris Lane Jones <chrislanejones@gmail.com>
2024-01-13 03:45:42 -0500

    TheatreJS - Medieval Town Setup

--- courses/4-7-theatrejs/src/App.jsx
+++ courses/4-7-theatrejs/src/App.jsx
@@ -1,14 +1,15 @@
 import { SoftShadows } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { UI } from "./UI";
 import { Experience } from "./components/Experience";
 
 import { getProject } from "@theatre/core";
-import { SheetProvider } from "@theatre/r3f";
+import { PerspectiveCamera, SheetProvider } from "@theatre/r3f";
 
 import extension from "@theatre/r3f/dist/extension";
 import studio from "@theatre/studio";
+import { editable as e } from "@theatre/r3f";
 
 const project = getProject("MedievalTownThreejs");
 const mainSheet = project.sheet("Main");
@@ -19,6 +20,8 @@ studio.extend(extension);
 function App() {
   const [currentScreen, setCurrentScreen] = useState("Intro");
   const [targetScreen, setTargetScreen] = useState("Home");
+
+  const cameraTargetRef = useRef();
 
   return (
     <>
@@ -34,6 +37,22 @@ function App() {
       >
         <SoftShadows />
         <SheetProvider sheet={mainSheet}>
+          <PerspectiveCamera
+            position={[5, 5, 10]}
+            fov={30}
+            near={1}
+            makeDefault
+            theatreKey="Camera"
+            lookAt={cameraTargetRef}
+          />
+          <e.mesh
+            theatreKey="Camera Target"
+            visible="editor"
+            ref={cameraTargetRef}
+          >
+            <octahedronBufferGeometry args={[0.1, 0]} />
+            <meshPhongMaterial color="yellow" />
+          </e.mesh>
           <Experience />
         </SheetProvider>
       </Canvas>

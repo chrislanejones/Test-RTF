e6612627ae909f555fdb9ca3966c51dd4781a022
Chris Lane Jones <chrislanejones@gmail.com>
2024-01-17 00:08:20 -0500

    TheatreJS - Adding Snapshot View

--- courses/4-7-theatrejs/src/components/Experience.jsx
+++ courses/4-7-theatrejs/src/components/Experience.jsx
@@ -2,8 +2,11 @@ import { Environment } from "@react-three/drei";
 import { MedievalFantasyBook } from "./MedievalFantasyBook";
 import { editable as e } from "@theatre/r3f";
 import { Autofocus, EffectComposer } from "@react-three/postprocessing";
+import { useRef } from "react";
 
 export const Experience = () => {
+  const focusTargetref = useRef(new Vector3(0, 0, 0));
+  const focusTargetVisualizerRef = useRef();
   return (
     <>
       <e.directionalLight
@@ -19,12 +22,21 @@ export const Experience = () => {
         <MedievalFantasyBook scale={0.1} envMapIntensity={0.3} />
       </e.group>
       <Environment preset="dawn" background blur={4} />
+      <e.mesh
+        theatreKey="FocusTarget"
+        ref={focusTargetVisualizerRef}
+        visible="editor"
+      >
+        <sphereBufferGeometry args={[0.01, 8, 8]} />
+        <meshBasicMaterial color="red" wireframe />
+      </e.mesh>
       <EffectComposer>
         <Autofocus
           smoothTime={0.1}
           debug={0.04}
           focusRange={0.002}
           bokehScale={8}
+          target={focusTargetref.current}
         />
       </EffectComposer>
     </>

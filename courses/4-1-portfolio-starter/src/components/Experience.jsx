83c63af3531145305f19a39d4adf016986ee4904
Chris Lane Jones <chrislanejones@gmail.com>
2024-01-24 07:50:02 -0500

    Portfolio-Starter - Scroll Movement System - Experience Chapter

--- courses/4-1-portfolio-starter/src/components/Experience.jsx
+++ courses/4-1-portfolio-starter/src/components/Experience.jsx
@@ -1,11 +1,23 @@
 import { Environment } from "@react-three/drei";
 import { Avatar } from "./Avatar";
+import { useRef } from "react";
 
 export const Experience = () => {
+  const sceneContainer = useRef();
   return (
     <>
       <Environment preset="sunset" />
       <Avatar />
+      <group ref={sceneContainer}>
+        {/* HOME */}
+        <group></group>
+        {/* SKILLS */}
+        <group></group>
+        {/* PROJECTS */}
+        <group></group>
+        {/* CONTACT */}
+        <group></group>
+      </group>
     </>
   );
 };

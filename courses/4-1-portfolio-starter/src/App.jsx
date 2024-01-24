e3f1e59e2722cfcd90e2ec6dc1927e9407a1d9c9
Chris Lane Jones <chrislanejones@gmail.com>
2024-01-24 07:42:03 -0500

    Portfolio-Starter - Scroll Movement System Setup

--- courses/4-1-portfolio-starter/src/App.jsx
+++ courses/4-1-portfolio-starter/src/App.jsx
@@ -1,5 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { ScrollControls } from "@react-three/drei";
+import { config } from "./config";
 
 function App() {
   return (
@@ -7,9 +9,15 @@ function App() {
       <Canvas camera={{ position: [0, 0.5, 5], fov: 42 }}>
         <color attach="background" args={["#f5f3ee"]} />
         <fog attach="fog" args={["#f5f3ee", 10, 50]} />
-        <group position-y={-1}>
-          <Experience />
-        </group>
+        <ScrollControls
+          pages={config.sections.length}
+          damping={0.1}
+          maxSpeed={0.2}
+        >
+          <group position-y={-1}>
+            <Experience />
+          </group>
+        </ScrollControls>
       </Canvas>
     </>
   );
---
+++ courses/4-1-portfolio-starter/src/config.jsx
@@ -0,0 +1,3 @@
+export const config = {
+  sections: ["home", "skills", "projects", "contact"],
+};

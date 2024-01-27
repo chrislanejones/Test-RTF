17a90a1184084d475e1550bdf4e73b6a92616764
Chris Lane Jones <chrislanejones@gmail.com>
2024-01-26 23:56:19 -0500

    Portfolio-Starter - Staging - Setting Up MacBookPro, Palmtree, and Star

--- courses/4-1-portfolio-starter/src/components/Experience.jsx
+++ courses/4-1-portfolio-starter/src/components/Experience.jsx
@@ -3,8 +3,11 @@ import { Avatar } from "./Avatar";
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import { Center, Float } from "@react-three/drei";
-import { SectionTitle } from "./SectionTitle";
+import { MacBookPro } from "./MacBookPro";
+import { PalmTree } from "./PalmTree";
 import { Star } from "./Star";
+
+import { SectionTitle } from "./SectionTitle";
 
 const SECTIONS_DISTANCE = 10;
 
@@ -24,6 +27,48 @@ export const Experience = () => {
       <group ref={sceneContainer}>
         {/* HOME */}
         <group>
+          <Star position-z={0} position-y={2.2} scale={0.3} />
+          <Float floatIntensity={2} speed={2}>
+            <MacBookPro
+              position-x={-1}
+              position-y={0.5}
+              position-z={0}
+              scale={0.3}
+              rotation-y={Math.PI / 4}
+            />
+          </Float>
+          <PalmTree
+            scale={0.018}
+            rotation-y={THREE.MathUtils.degToRad(140)}
+            position={[4, 0, -5]}
+          />
+          <Float floatIntensity={0.6}>
+            <Center disableY disableZ>
+              <SectionTitle
+                size={0.8}
+                position-y={1.6}
+                position-z={-3}
+                bevelEnabled
+                bevelThickness={0.3}
+              >
+                {config.home.title}
+              </SectionTitle>
+            </Center>
+          </Float>
+          <Center disableY disableZ>
+            <SectionTitle
+              size={1.2}
+              position-x={-2.6}
+              position-z={-3}
+              bevelEnabled
+              bevelThickness={0.3}
+              rotation-y={Math.PI / 10}
+            >
+              {config.home.subtitle}
+            </SectionTitle>
+          </Center>
+        </group>
+        <group>
           <SectionTitle position-x={0.5}>HOME</SectionTitle>
         </group>
         {/* SKILLS */}
@@ -38,8 +83,6 @@ export const Experience = () => {
         <group position-z={3 * SECTIONS_DISTANCE}>
           <SectionTitle position-x={0.5}>CONTACT</SectionTitle>
         </group>
-        <Star position-z={0} position-y={2.2} scale={0.3} />
-        <Star position-z={0} position-y={2.2} scale={0.3} />
       </group>
     </>
   );

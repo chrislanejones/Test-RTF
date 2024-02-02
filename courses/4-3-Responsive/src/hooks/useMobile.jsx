aa7cc428778fafc33f20234ee2ace09eb2ebd3c4
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-02 00:15:07 -0500

    Responsive - useMobile - useState to Scale and useEffect to Listen

--- courses/4-3-Responsive/src/hooks/useMobile.jsx
+++ courses/4-3-Responsive/src/hooks/useMobile.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
 const REFERENCE_WIDTH = 1920;
@@ -7,6 +8,23 @@ export const useMobile = () => {
   const [scaleFactor, setScaleFactor] = useState(
     window.innerWidth / REFERENCE_WIDTH
   );
+  const [isMobile, setIsMobile] = useState(
+    window.innerWidth <= MOBILE_THRESHOLD
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScaleFactor(window.innerWidth / REFERENCE_WIDTH);
+      if (window.innerWidth <= MOBILE_THRESHOLD) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize, handleResize");
+  }, []);
+
   return {
     scaleFactor,
     isMobile,

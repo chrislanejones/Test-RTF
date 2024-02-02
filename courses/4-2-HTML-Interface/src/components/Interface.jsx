27b0cc76c5dc1b8f5a8b4bdf7aa23ecf2ea3ceeb
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-02 03:02:05 -0500

    HTML Interface Portfolio - Home Scroll Indicator

--- courses/4-2-HTML-Interface/src/components/Interface.jsx
+++ courses/4-2-HTML-Interface/src/components/Interface.jsx
@@ -1,9 +1,47 @@
+import { useScroll } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { motion } from "framer-motion";
+import { useState } from "react";
+
 export const Interface = () => {
+  const [hasScrolled, SetHasScrolled] = useState(false);
+  const scrollData = useScroll();
+
+  useFrame(() => {
+    SetHasScrolled(scrollData.offset > 0);
+  });
+
   return (
     <div className="interface">
       <div className="sections">
         {/* HOME */}
-        <section className="section section--bottom">HOME</section>
+        <section className="section section--bottom">
+          <motion.div
+            className="scroll-down"
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: hasScrolled ? 0 : 1,
+            }}
+          >
+            <motion.div
+              className="scroll-down__wheel"
+              initial={{
+                translateY: 0,
+              }}
+              animate={{
+                translateY: 4,
+              }}
+              transition={{
+                duration: 0.4,
+                repeatDelay: 0.5,
+                repeatType: "reverse",
+                repeat: Infinity,
+              }}
+            ></motion.div>
+          </motion.div>
+        </section>
         {/* SKILLS */}
         <section className="section section--right">SKILLS</section>
         {/* PROJECTS */}

bd7eb10ac7e2dc1c4224ce674f5524028447babc
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-02 02:37:48 -0500

    HTML Interface Portfolio - Interface - Interface.jsx

---
+++ courses/4-2-HTML-Interface/src/components/Interface.jsx
@@ -0,0 +1,16 @@
+export const Interface = () => {
+  return (
+    <div className="interface">
+      <div className="sections">
+        {/* HOME */}
+        <section className="section section--bottom">HOME</section>
+        {/* SKILLS */}
+        <section className="section section--right">SKILLS</section>
+        {/* PROJECTS */}
+        <section className="section section--left">PROJECTS</section>
+        {/* CONTACT */}
+        <section className="section section--left">CONTACT</section>
+      </div>
+    </div>
+  );
+};

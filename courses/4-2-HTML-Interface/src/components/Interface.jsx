0496f5ab455a3fd5bcefdc1ebd899c6f2045ddb9
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-03 04:34:03 -0500

    HTML Interface Portfolio - Skills - Configuration Added to Config.JSX

--- courses/4-2-HTML-Interface/src/components/Interface.jsx
+++ courses/4-2-HTML-Interface/src/components/Interface.jsx
@@ -43,7 +43,61 @@ export const Interface = () => {
           </motion.div>
         </section>
         {/* SKILLS */}
-        <section className="section section--right">SKILLS</section>
+        <section className="section section--right">
+          <motion.div
+            className="skills"
+            whileInView={"visible"}
+            initial={{
+              opacity: 0,
+            }}
+            variants={{
+              visible: {
+                opacity: 1,
+              },
+            }}
+          >
+            {config.skills.map((skill, idx) => (
+              <motion.div
+                key={skill.name}
+                className="skill"
+                initial={{ opacity: 0 }}
+                variants={{
+                  visible: {
+                    opacity: 1,
+                  },
+                }}
+                transition={{
+                  duration: 1,
+                  delay: idx * 0.62,
+                }}
+              >
+                <div className="skill__label">
+                  <img
+                    className="skill__label__image"
+                    src={skill.icon}
+                    alt={skill.name}
+                  />
+                  <h2 className="skill__label__name">{skill.name}</h2>
+                </div>
+                <div className="skill__level">
+                  <motion.div
+                    className="skill__level__bar"
+                    initial={{ width: 0 }}
+                    variants={{
+                      visible: {
+                        width: `${skill.level}%`,
+                      },
+                    }}
+                    transition={{
+                      duration: 1,
+                      delay: idx * 0.62,
+                    }}
+                  ></motion.div>
+                </div>
+              </motion.div>
+            ))}
+          </motion.div>
+        </section>
         {/* PROJECTS */}
         <section className="section section--left">PROJECTS</section>
         {/* CONTACT */}

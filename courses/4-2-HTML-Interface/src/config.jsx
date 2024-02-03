0685de63716b74f1c96fcf0f2bbab0ca62cc0392
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-03 04:27:32 -0500

    HTML Interface Portfolio - Skills - Skills Added to Config.jsx

--- courses/4-2-HTML-Interface/src/config.jsx
+++ courses/4-2-HTML-Interface/src/config.jsx
@@ -5,6 +5,28 @@ export const config = {
     midtitle: "Lane",
     subtitle: "Jones",
   },
+  skills: [
+    {
+      name: "React",
+      icon: "icons/react-native.png",
+      level: 80,
+    },
+    {
+      name: "JavaScript",
+      icon: "icons/javascript.png",
+      level: 70,
+    },
+    {
+      name: "Three.js",
+      icon: "icons/threejs.png",
+      level: 70,
+    },
+    {
+      name: "Blender",
+      icon: "icons/blender.png",
+      level: 50,
+    },
+  ],
   contact: {
     mail: "chrilanejones@gmail.com",
   },

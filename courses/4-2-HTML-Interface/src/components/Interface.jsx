e19f594c5c4b7d65567e363df88e307511f1a225
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-04 22:04:40 -0500

    HTML Interface Portfolio - Projects - Interface.jsx - Iterate Code

--- courses/4-2-HTML-Interface/src/components/Interface.jsx
+++ courses/4-2-HTML-Interface/src/components/Interface.jsx
@@ -100,7 +100,51 @@ export const Interface = () => {
           </motion.div>
         </section>
         {/* PROJECTS */}
-        <section className="section section--left">PROJECTS</section>
+        <section className="section section--left">
+          <motion.div
+            className="projects"
+            whileInView={"visible"}
+            initial={{
+              opacity: 0,
+            }}
+            variants={{
+              visible: {
+                opacity: 1,
+              },
+            }}
+          >
+            {config.projects.map((project, idx) => (
+              <motion.div
+                key={project.name}
+                className="project"
+                initial={{ opacity: 0 }}
+                variants={{
+                  visible: {
+                    opacity: 1,
+                  },
+                }}
+                transition={{
+                  duration: 1,
+                  delay: idx * 0.5,
+                }}
+              >
+                <a href={project.link} target="_blank">
+                  <img
+                    className="project__image"
+                    src={project.image}
+                    alt={project.name}
+                  />
+                  <div className="project__details">
+                    <h2 className="project__details__name">{project.name}</h2>
+                    <p className="project__details__description">
+                      {project.description}
+                    </p>
+                  </div>
+                </a>
+              </motion.div>
+            ))}
+          </motion.div>
+        </section>
         {/* CONTACT */}
         <section className="section section--left">CONTACT</section>
       </div>

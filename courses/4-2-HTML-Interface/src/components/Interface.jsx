00787758f41a96c756bc1c4d7c5b40debb3b74e2
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-05 23:28:53 -0500

    HTML Interface Portfolio - Contact - Experience Motion.div Added

--- courses/4-2-HTML-Interface/src/components/Interface.jsx
+++ courses/4-2-HTML-Interface/src/components/Interface.jsx
@@ -151,7 +151,46 @@ export const Interface = () => {
           </motion.div>
         </section>
         {/* CONTACT */}
-        <section className="section section--left">CONTACT</section>
+        <section className="section section--left">
+          <motion.div
+            className="contact"
+            whileInView={"visable"}
+            initial={{
+              opacity: 0,
+            }}
+            variants={{
+              visable: {
+                opacity: 1,
+              },
+            }}
+          >
+            <h1 className="contact__name">{config.contact.name}</h1>
+            <p className="contact__address">{config.contact.address}</p>
+            <div className="contact__socials">
+              <a href={config.contact.socials.linkedin} target="_blank">
+                <img
+                  className="contact__socials__icon"
+                  src="icons/linkedin.png"
+                  alt="linkedin"
+                />
+              </a>
+              <a href={config.contact.socials.twitter} target="_blank">
+                <img
+                  className="contact__socials__icon"
+                  src="icons/twitter.png"
+                  alt="twitter"
+                />
+              </a>
+              <a href={`mailto:${config.contact.mail}`} target="_blank">
+                <img
+                  className="contact__socials__icon"
+                  src="icons/email.png"
+                  alt="email"
+                />
+              </a>
+            </div>
+          </motion.div>
+        </section>
       </div>
     </div>
   );

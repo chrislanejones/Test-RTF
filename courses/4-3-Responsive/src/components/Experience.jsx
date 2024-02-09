688d0efc536b9fa16858dfe39028906347d71e0e
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-09 00:01:12 -0500

    Responsive - Horizontal Movement - useAvatar

--- courses/4-3-Responsive/src/components/Experience.jsx
+++ courses/4-3-Responsive/src/components/Experience.jsx
@@ -53,27 +53,6 @@ export const Experience = () => {
     );
   });
 
-  useFrame(() => {
-    const scrollDelta = scrollData.offset - lastScroll.current;
-    let rotationTarget = 0;
-    if (Math.abs(scrollDelta) > 0.00001) {
-      setAnimation("Walking");
-      if (scrollDelta > 0) {
-        rotationTarget = isMobile ? Math.PI / 2 : 0;
-      } else {
-        rotationTarget = isMobile ? -Math.PI / 2 : Math.PI;
-      }
-    } else {
-      setAnimation("Idle");
-    }
-    group.current.rotation.y = THREE.MathUtils.lerp(
-      group.current.rotation.y,
-      rotationTarget,
-      0.1
-    );
-    lastScroll.current = scrollData.offset;
-  });
-
   useEffect(() => {
     const handleHashChange = () => {
       const sectionIndex = config.sections.indexOf(

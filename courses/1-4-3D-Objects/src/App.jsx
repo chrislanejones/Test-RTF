e232a089fc82d87467de253cfb45385b1d20b286
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-23 01:39:03 -0500

    Objects - Mesh Material Cube

--- courses/1-4-3D-Objects/src/App.jsx
+++ courses/1-4-3D-Objects/src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <Canvas camera={{ position: [0, 3, 3] }} style={{ background: "#5aa38b" }}>
       <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+        <boxGeometry args={[1, 1, 1]} />
+        <meshBasicMaterial color={0x00ff00} />
       </mesh>
     </Canvas>
   );

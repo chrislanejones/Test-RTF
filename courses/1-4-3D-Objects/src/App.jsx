14399d4bfaf2307a63d1593f47905a29b99da6c3
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-23 01:51:13 -0500

    Objects - Three Materials With Light

--- courses/1-4-3D-Objects/src/App.jsx
+++ courses/1-4-3D-Objects/src/App.jsx
@@ -1,8 +1,10 @@
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
 function App() {
   return (
     <Canvas camera={{ position: [0, 3, 3] }} style={{ background: "#5aa38b" }}>
+      <OrbitControls />
       <mesh position={[0, 0, 0]}>
         <boxGeometry args={[1, 1, 1]} />
         <meshBasicMaterial color={0x00ff00} />
@@ -19,6 +21,13 @@ function App() {
         <cylinderGeometry args={[1, 1, 2, 32]} />
         <meshNormalMaterial color={0x00ff00} />
       </mesh>
+      <mesh position={[0, 0, 2]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="green" />
+      </mesh>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 0, 3]} intensity={1} />
+      <directionalLight position={[0, 3, 3]} intensity={0.5} />
     </Canvas>
   );
 }

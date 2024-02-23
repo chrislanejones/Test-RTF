096e1e9a4ee85275786197b67f98479904d53f7b
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-23 01:46:42 -0500

    Objects - Three Materials in Position

--- courses/1-4-3D-Objects/src/App.jsx
+++ courses/1-4-3D-Objects/src/App.jsx
@@ -3,9 +3,21 @@ import { Canvas } from "@react-three/fiber";
 function App() {
   return (
     <Canvas camera={{ position: [0, 3, 3] }} style={{ background: "#5aa38b" }}>
-      <mesh>
+      <mesh position={[0, 0, 0]}>
         <boxGeometry args={[1, 1, 1]} />
         <meshBasicMaterial color={0x00ff00} />
+      </mesh>
+      <mesh position={[2, 0, 0]}>
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshNormalMaterial color={0x00ff00} />
+      </mesh>
+      <mesh position={[0, 0, -1]}>
+        <planeGeometry args={[5, 5]} />
+        <meshNormalMaterial color={0x00ff00} />
+      </mesh>
+      <mesh position={[-2, 0, 0]}>
+        <cylinderGeometry args={[1, 1, 2, 32]} />
+        <meshNormalMaterial color={0x00ff00} />
       </mesh>
     </Canvas>
   );

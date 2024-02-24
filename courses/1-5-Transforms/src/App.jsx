d35335819dc23faaca6e71e403794958c6232de3
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-24 00:12:00 -0500

    Transforms - Rotation and Math.PI

--- courses/1-5-Transforms/src/App.jsx
+++ courses/1-5-Transforms/src/App.jsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-
+import * as THREE from "three";
 function App() {
   return (
     <Canvas camera={{ position: [0, 3, 8] }} style={{ background: "#20222B" }}>
@@ -8,15 +8,19 @@ function App() {
       <directionalLight position={[0, 3, 5]} intensity={0.5} />
 
       {/* Objects */}
-      <mesh position={[-3, 0, 0]} scale={[0.5, 0.5, 0.5]}>
+      <mesh
+        position={[-3, 0, 0]}
+        scale={[1, 0.5, 0.5]}
+        rotation={[THREE.MathUtils.degToRad(45), 0, 0]}
+      >
         <boxGeometry />
         <meshStandardMaterial color="red" />
       </mesh>
-      <mesh scale-y={4}>
+      <mesh scale-y={4} rotation-y={Math.PI / 4}>
         <boxGeometry />
         <meshStandardMaterial color="blue" />
       </mesh>
-      <mesh position={[3, 0, 0]} scale-z={3}>
+      <mesh position={[3, 0, 0]} scale-z={3} rotation={[Math.PI / 4, 0, 0]}>
         <boxGeometry />
         <meshStandardMaterial color="green" />
       </mesh>

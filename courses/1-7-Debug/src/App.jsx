83b7e1575d441d88b079c608593c0d9248dcb2c7
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-28 00:54:49 -0500

    Debug - useHelper - Box

--- courses/1-7-Debug/src/App.jsx
+++ courses/1-7-Debug/src/App.jsx
@@ -1,9 +1,14 @@
-import { Grid } from "@react-three/drei";
+import { Grid, useHelper } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useRef } from "react";
+import * as THREE from "three";
+s;
 
 const Box = () => {
+  const ref = useRef();
+  useHelper(ref, THREE.BoxHelper, "red");
   return (
-    <mesh>
+    <mesh ref={ref}>
       <boxGeometry />
       <meshBasicMaterial color="white" transparent opacity={0} />
     </mesh>

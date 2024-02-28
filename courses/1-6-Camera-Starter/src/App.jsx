02fef8d0bd40e24d9ecf806bca2e6c5ce925c163
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-28 00:24:05 -0500

    Camera - OrthographicCamera - Fixed Final

--- courses/1-6-Camera-Starter/src/App.jsx
+++ courses/1-6-Camera-Starter/src/App.jsx
@@ -2,6 +2,7 @@ import {
   Environment,
   OrbitControls,
   PerspectiveCamera,
+  OrthographicCamera,
 } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { City } from "./components/City";
@@ -9,7 +10,25 @@ import { City } from "./components/City";
 function App() {
   return (
     <Canvas>
-      <OrthographicCamera makeDefault position={[1, 1, 1]} zoom={100} />
+      {/* <OrthographicCamera
+        makeDefault
+        position={[1, 1, 1]}
+        left={-2}
+        right={2}
+        top={2}
+        bottom={-2}
+      /> */}
+
+      <OrthographicCamera
+        makeDefault
+        position={[1, 1, 1]}
+        left={-2 * (window.innerWidth / window.innerHeight)}
+        right={2 * (window.innerWidth / window.innerHeight)}
+        top={2}
+        bottom={-2}
+        near={-5}
+      />
+
       <OrbitControls />
       {/* IGNORE FOR NOW */}
       <City />

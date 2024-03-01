adb517ae0f2028be78696a766d40b24f1d51b59f
Chris Lane Jones <chrislanejones@gmail.com>
2024-03-01 01:46:32 -0500

    Shadow - AccumulativeShadows, RandomizedLight, and Force Sphere in Cube

--- courses/1-9-Shadows-Starter/src/App.jsx
+++ courses/1-9-Shadows-Starter/src/App.jsx
@@ -1,7 +1,9 @@
 import {
+  AccumulativeShadows,
   BakeShadows,
   ContactShadows,
   OrbitControls,
+  RandomizedLight,
   SoftShadows,
 } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
@@ -11,36 +13,52 @@ function App() {
   const { cubeInAir } = useControls({
     cubeInAir: false,
   });
+  const { sphereMove } = useControls({
+    sphereMove: false,
+  });
   return (
     <>
       <Canvas
+        shadows
         camera={{ position: [0, 3, 3] }}
         style={{ background: "#20222B" }}
       >
-        <ContactShadows
-          frames={1}
-          position-y={-0.49}
-          opacity={0.5}
-          blur={2}
-          color={"pink"}
-          scale={10}
-        />
-        <SoftShadows />
-        <BakeShadows />
         <OrbitControls />
+        <OrbitControls />
+        <AccumulativeShadows
+          temporal
+          frames={35}
+          alphaTest={0.85}
+          scale={5}
+          position={[0, -0.499, 0]}
+          color="#EFBD4E"
+        >
+          <RandomizedLight
+            amount={4}
+            radius={9}
+            intensity={0.55}
+            ambient={0.25}
+            position={[5, 5, -10]}
+          />
+          <RandomizedLight
+            amount={4}
+            radius={5}
+            intensity={0.25}
+            ambient={0.55}
+            position={[-5, 5, -9]}
+          />
+        </AccumulativeShadows>
+
         <ambientLight intensity={0.5} />
-        <directionalLight
-          position={[5, 5, 5]}
-          intensity={0.5}
+        <directionalLight position={[5, 5, 5]} intensity={0.5} />
+        <directionalLight position={[-5, 5, 5]} intensity={0.5} color="red" />
+        <mesh
+          position-y={1}
+          position-x={sphereMove ? 0.5 : 1}
+          position-z={sphereMove ? 0.5 : 1}
+          rotation-y={Math.PI / 2}
           castShadow
-          shadow-mapSize={[2048, 2048]}
-        />
-        <directionalLight
-          position={[5, 5, 5]}
-          intensity={0.5}
-          castShadow
-        ></directionalLight>
-        <mesh position={[1, 1, 1]} castShadow>
+        >
           <sphereGeometry args={[0.5, 32, 32]} />
           <meshStandardMaterial color="white" />
         </mesh>

7f43bc276dff271db966f634a41f548542069977
Chris Lane Jones <chrislanejones@gmail.com>
2024-03-01 00:23:09 -0500

    Shadow - Shadows For Object Different Areas - Shadow Camera

--- courses/1-9-Shadows-Starter/src/App.jsx
+++ courses/1-9-Shadows-Starter/src/App.jsx
@@ -17,25 +17,42 @@ function App() {
           castShadow
           shadow-mapSize={[2048, 2048]}
         />
-        <directionalLight
-          position={[-5, 5, 5]}
-          intensity={0.5}
-          color="red"
-          castShadow
-        />
+        <directionalLight position={[5, 5, 5]} intensity={0.5} castShadow>
+          <orthographicCamera
+            attach="shadow-camera"
+            args={[-10, 10, 10, -10]}
+          />
+        </directionalLight>
         <mesh position={[1, 1, 1]} castShadow>
           <sphereGeometry args={[0.5, 32, 32]} />
           <meshStandardMaterial color="white" />
         </mesh>
-        ;
+
         <mesh rotation-y={Math.PI / 4} castShadow receiveShadow>
           <boxGeometry />
           <meshStandardMaterial color="white" />
         </mesh>
+
         <mesh rotation-x={-Math.PI / 2} position-y={-0.5} receiveShadow>
           <planeGeometry args={[5, 5]} />
           <meshStandardMaterial color="white" />
         </mesh>
+        <group position-x={8}>
+          <mesh position={[1, 1, 1]} castShadow>
+            <sphereGeometry args={[0.5, 32, 32]} />
+            <meshStandardMaterial color="white" />
+          </mesh>
+
+          <mesh rotation-y={Math.PI / 4} castShadow receiveShadow>
+            <boxGeometry />
+            <meshStandardMaterial color="white" />
+          </mesh>
+
+          <mesh rotation-x={-Math.PI / 2} position-y={-0.5} receiveShadow>
+            <planeGeometry args={[5, 5]} />
+            <meshStandardMaterial color="white" />
+          </mesh>
+        </group>
       </Canvas>
     </>
   );

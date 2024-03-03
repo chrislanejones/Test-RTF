71c608953f633d1ee46a9300f2f1f14ea21ce768
Chris Lane Jones <chrislanejones@gmail.com>
2024-03-03 03:27:47 -0500

    Controls - OrbitControls - Pan, maxPolarAngle, maxAzimuthAngle

---
+++ courses/2-3-Controls/src/App.jsx
@@ -0,0 +1,26 @@
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Lighthouse } from "./components/Lighthouse";
+function App() {
+  return (
+    <>
+      <Canvas
+        style={{ background: "#6a6a6a" }}
+        camera={{ position: [-1.5, 3, 10], fov: 42 }}
+      >
+        <OrbitControls
+          enablePan={false}
+          maxPolarAngle={Math.PI / 2}
+          minAzimuthAngle={-Math.PI / 2}
+          maxAzimuthAngle={Math.PI / 2}
+          minDistance={3}
+          maxDistance={10}
+        />
+        <Lighthouse position-y={-1} scale={[0.2, 0.2, 0.2]} />
+        <Environment preset="sunset" />
+      </Canvas>
+    </>
+  );
+}
+
+export default App;

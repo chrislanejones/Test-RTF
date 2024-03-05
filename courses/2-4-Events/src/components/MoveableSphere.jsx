9a1b59d0306353d82fe024bfd0b3470607860be8
Chris Lane Jones <chrislanejones@gmail.com>
2024-03-04 22:28:40 -0500

    Events - Mouse -  onClick

--- courses/2-4-Events/src/components/MoveableSphere.jsx
+++ courses/2-4-Events/src/components/MoveableSphere.jsx
@@ -2,14 +2,20 @@ import { useState } from "react";
 
 export const MoveableSphere = (props) => {
   const [hovered, setHovered] = useState(false);
+  const [selected, setSelected] = useState(false);
+  let color = hovered ? "pink" : "white";
+  if (selected) {
+    color = "hotpink";
+  }
   return (
     <mesh
       {...props}
       onPointerEnter={() => setHovered(true)}
       onPointerLeave={() => setHovered(false)}
+      onClick={() => setSelected(!selected)}
     >
       <sphereGeometry args={[0.5, 64, 64]} />
-      <meshStandardMaterial color={"white"} />
+      <meshStandardMaterial color={color} />
     </mesh>
   );
 };

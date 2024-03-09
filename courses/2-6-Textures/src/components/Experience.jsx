f11639a090f80e1e48112c20ba7031f568836859
Chris Lane Jones <chrislanejones@gmail.com>
2024-03-09 02:57:57 -0500

    Texture - useTexture - Shrunk Texture, wrapS and wrapT

--- courses/2-6-Textures/src/components/Experience.jsx
+++ courses/2-6-Textures/src/components/Experience.jsx
@@ -1,7 +1,11 @@
 import { useTexture } from "@react-three/drei";
+import * as THREE from "three";
 
 export const Experience = () => {
   const texture = useTexture("textures/PavingStones130_1K_Color.jpg");
+
+  texture.repeat.set(3, 3);
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
 
   return (
     <>

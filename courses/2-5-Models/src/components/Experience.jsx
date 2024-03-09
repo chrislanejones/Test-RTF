688c02c42e47a64921ae9e00938250ca0b3cbbfa
Chris Lane Jones <chrislanejones@gmail.com>
2024-03-08 23:48:54 -0500

    Models - Animations - Forgot to Pass Props

--- courses/2-5-Models/src/components/Experience.jsx
+++ courses/2-5-Models/src/components/Experience.jsx
@@ -1,4 +1,5 @@
 import { useFBX, useGLTF, Gltf } from "@react-three/drei";
+import { Fish } from "./Fish";
 
 export const Experience = () => {
   const dino = useFBX("models/Dino.fbx");
@@ -11,7 +12,7 @@ export const Experience = () => {
         position-z={-1}
         rotation-y={Math.PI / 2}
       />
-      <Gltf src="models/Fish.gltf" />
+      <Fish />
       <primitive object={dino} scale={0.01} position-x={-3} />
     </>
   );

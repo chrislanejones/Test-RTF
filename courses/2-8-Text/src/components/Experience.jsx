ca7d87da722f6ff43713c9da34ab8e4490da4e17
Chris Lane Jones <chrislanejones@gmail.com>
2024-03-12 02:00:02 -0400

    Text - 2D Text - Text Anchor and Position

--- courses/2-8-Text/src/components/Experience.jsx
+++ courses/2-8-Text/src/components/Experience.jsx
@@ -21,6 +21,16 @@ export const Experience = () => {
         </Text>
       </group>
       <group position={[1.5, 0, 0]} rotation-y={-Math.PI / 4}>
+        <group position-y={3}>
+          <Text fontSize={0.2} anchorY={"bottom"}>
+            Link
+            <meshStandardMaterial color={"black"} />
+          </Text>
+          <Text fontSize={0.2} anchorY={"top"}>
+            Zelda personal hero
+            <meshStandardMaterial color={"grey"} />
+          </Text>
+        </group>
         <Character />
       </group>
       <ContactShadows opacity={0.42} scale={42} far={42} />

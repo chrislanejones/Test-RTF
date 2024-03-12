20fc8ef1f225fb466e9df416a8a27b605cdb4af3
Chris Lane Jones <chrislanejones@gmail.com>
2024-03-11 23:54:42 -0400

    HTML - Ikea Showroom - HTML Elements

--- courses/2-6-HTML/src/components/Experience.jsx
+++ courses/2-6-HTML/src/components/Experience.jsx
@@ -5,30 +5,42 @@ const sceneItems = [
     model: "Japanese Door.glb",
     position: [0, -0.88, -1.2],
     scale: 1.8,
+    name: "Hemnes",
+    price: 300,
   },
   {
     model: "Counter Sink.glb",
     position: [1.3, 0, -2.8],
+    name: "Lillången",
+    price: 450,
   },
   {
     model: "Chopping board.glb",
     position: [2.8, 1.9, -2.8],
     scale: 0.5,
+    name: "Skogsta",
+    price: 25,
   },
   {
     model: "Fridge.glb",
     position: [-2.1, 0, -3],
+    name: "Lagan",
+    price: 600,
   },
   {
     model: "Table.glb",
     position: [-1, 0, 2],
     scale: [1, 1, 1],
+    name: "Lerhamn",
+    price: 80,
   },
   {
     model: "Dango.glb",
     position: [-1.4, 1.64, 2],
     scale: 0.72,
     rotation: [0, Math.PI / 6, 0],
+    name: "Dango",
+    price: 4,
   },
 ];
 

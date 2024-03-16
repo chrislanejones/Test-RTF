2f49f7ef29e8876e11a764f026dfb0eb2ef30db3
Chris Lane Jones <chrislanejones@gmail.com>
2024-03-15 23:53:41 -0400

    Scroll - Scroll component- viewport.height

--- courses/2-10-Scroll/src/components/Experience.jsx
+++ courses/2-10-Scroll/src/components/Experience.jsx
@@ -1,7 +1,13 @@
-import { Environment, OrbitControls, Text3D, useGLTF } from "@react-three/drei";
+import {
+  Environment,
+  OrbitControls,
+  Scroll,
+  Text3D,
+  useGLTF,
+} from "@react-three/drei";
 import { Panda } from "./Panda";
-
 import { foodItems } from "../App";
+import { useThree } from "@react-three/fiber";
 
 export const Experience = () => {
   return (
@@ -36,19 +42,24 @@ export const Experience = () => {
         </Text3D>
         <Panda position={[2.5, 0, -5]} rotation-y={-Math.PI / 6} />
       </group>
-      {foodItems.map((foodItem, idx) => (
-        <FoodItem key={idx} {...foodItem} />
-      ))}
+      <Scroll>
+        {foodItems.map((foodItem, idx) => (
+          <FoodItem key={idx} {...foodItem} />
+        ))}
+      </Scroll>
     </>
   );
 };
 
 const FoodItem = ({ model, page }) => {
   const gltf = useGLTF(model);
-
+  const viewport = useThree((state) => state.viewport);
   return (
     <group>
-      <primitive object={gltf.scene} position={[0, -page, 0]} />
+      <primitive
+        object={gltf.scene}
+        position={[0, -viewport.height * page, 0]}
+      />
     </group>
   );
 };

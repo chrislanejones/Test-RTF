3c62e9f7073a6d01c4721fe20a89daf021907a56
Chris Lane Jones <chrislanejones@gmail.com>
2024-03-19 02:41:16 -0400

    Animations - Spring Based Animations - Framer Motion - useEffect on Teeth

--- courses/2-11-Animations/src/components/Teeth.jsx
+++ courses/2-11-Animations/src/components/Teeth.jsx
@@ -1,10 +1,35 @@
+import { motion } from "framer-motion-3d";
+import { useEffect, useState } from "react";
+
 export const Teeth = () => {
+  const [variant, setVariant] = useState("closed");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setVariant((prev) => (prev === "closed" ? "open" : "closed"));
+    }, 1500);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <group>
-      <mesh position-x={-1} position-y={-1}>
+    <motion.group animate={variant}>
+      <motion.mesh
+        position-x={-1}
+        position-y={-1}
+        animate={{
+          y: 0,
+        }}
+        transition={{
+          repeat: Infinity,
+          repeatDelay: 1,
+        }}
+      >
         <coneGeometry args={[0.5, 1, 4]} />
         <meshStandardMaterial color="#ffffff" />
-      </mesh>
+      </motion.mesh>
       <mesh position-x={0} position-y={1} rotation-x={Math.PI}>
         <coneGeometry args={[0.5, 1, 4]} />
         <meshStandardMaterial color="#ffffff" />
@@ -13,6 +38,6 @@ export const Teeth = () => {
         <coneGeometry args={[0.5, 1, 4]} />
         <meshStandardMaterial color="#ffffff" />
       </mesh>
-    </group>
+    </motion.group>
   );
 };

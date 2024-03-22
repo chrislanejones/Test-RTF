794be922eae348ba7b6cfff986e32004aa06dedc
Chris Lane Jones <chrislanejones@gmail.com>
2024-03-21 23:57:32 -0400

    Physics - Physics World - Rigidbody

--- courses/3-1-Physics/src/components/Player.jsx
+++ courses/3-1-Physics/src/components/Player.jsx
@@ -1,8 +1,12 @@
+import { RigidBody } from "@react-three/rapier";
+
 export const Player = () => {
   return (
-    <mesh position-y={0.5} castShadow>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="hotpink" />
-    </mesh>
+    <RigidBody>
+      <mesh position-y={0.5} castShadow>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="hotpink" />
+      </mesh>
+    </RigidBody>
   );
 };

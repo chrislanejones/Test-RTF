2da77201d06b95c5e61f81b240a0509be4ba3281
Chris Lane Jones <chrislanejones@gmail.com>
2024-03-22 03:58:03 -0400

    Physics - Forces - KeyboardControls - applyImpulse to setLinvel

--- courses/3-1-Physics/src/components/Player.jsx
+++ courses/3-1-Physics/src/components/Player.jsx
@@ -4,34 +4,35 @@ import { useKeyboardControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import { Vector3 } from "three";
-const MOVEMENT_SPEED = 0.5;
+
+const MOVEMENT_SPEED = 5;
 
 export const Player = () => {
-  const rb = useRef();
   const [, get] = useKeyboardControls();
-  const impulse = new Vector3();
+  const rb = useRef();
+  const vel = new Vector3();
   useFrame(() => {
-    impulse.x = 0;
-    impulse.y = 0;
-    impulse.z = 0;
+    vel.x = 0;
+    vel.y = 0;
+    vel.z = 0;
     if (get()[Controls.forward]) {
-      impulse.z -= MOVEMENT_SPEED;
+      vel.z -= MOVEMENT_SPEED;
     }
     if (get()[Controls.back]) {
-      impulse.z += MOVEMENT_SPEED;
+      vel.z += MOVEMENT_SPEED;
     }
     if (get()[Controls.left]) {
-      impulse.x -= MOVEMENT_SPEED;
+      vel.x -= MOVEMENT_SPEED;
     }
     if (get()[Controls.right]) {
-      impulse.x += MOVEMENT_SPEED;
+      vel.x += MOVEMENT_SPEED;
     }
     if (get()[Controls.jump]) {
     }
-    rb.current.applyImpulse(impulse, true);
+    rb.current.setLinvel(vel, true);
   });
 
-  <RigidBody ref={rb}>
+  <RigidBody ref={rb} lockRotations>
     <mesh position-y={0.5} castShadow>
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color="hotpink" />

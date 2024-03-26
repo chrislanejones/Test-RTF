8b17e6db16e027996b6c896962e56ccbb0e0f340
Chris Lane Jones <chrislanejones@gmail.com>
2024-03-25 23:59:21 -0400

    Physics - Third Person Controller - Lerp - Vec3 LookAt

--- courses/3-1-Physics/src/components/Player.jsx
+++ courses/3-1-Physics/src/components/Player.jsx
@@ -1,4 +1,4 @@
-import { RigidBody } from "@react-three/rapier";
+import { RigidBody, vec3 } from "@react-three/rapier";
 import { Controls } from "../App";
 import { PerspectiveCamera, useKeyboardControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
@@ -11,10 +11,14 @@ const JUMP_FORCE = 8;
 
 export const Player = () => {
   const rb = useRef();
+  const camera = useRef();
+  const cameraTarget = useRef(new Vector3(0, 0, 0));
   const [, get] = useKeyboardControls();
   const inTheAir = useRef(false);
   const vel = new Vector3();
   useFrame(() => {
+    cameraTarget.current.lerp(vec3(rb.current.translation()), 0.5);
+    camera.current.lookAt(cameraTarget.current);
     vel.x = 0;
     vel.y = 0;
     vel.z = 0;
@@ -39,6 +43,7 @@ export const Player = () => {
     }
     rb.current.setLinvel(vel, true);
   });
+
   return (
     <RigidBody
       ref={rb}
@@ -50,7 +55,7 @@ export const Player = () => {
       }}
       gravityScale={2.5}
     >
-      <PerspectiveCamera makeDefault position={[0, 5, 8]} />
+      <PerspectiveCamera makeDefault position={[0, 5, 8]} ref={camera} />
       <mesh position-y={0.5} castShadow>
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color="hotpink" />

a633a39baeac32640f8ec41ab7adf262ece4e4a7
Chris Lane Jones <chrislanejones@gmail.com>
2024-03-29 23:59:35 -0400

    Staging - Stage - Background - Ground Added

--- courses/3-2-Staging/src/components/Experience.jsx
+++ courses/3-2-Staging/src/components/Experience.jsx
@@ -16,6 +16,11 @@ export const Experience = () => {
       <Stage intensity={0.4} preset={"upfront"} environment={"studio"}>
         <TeslaModel3 scale={0.012} position-z={0.6} />
       </Stage>
+
+      <mesh position={[0, -1.18, 0]} rotation-x={-Math.PI / 2}>
+        <planeGeometry args={[100, 100]} />
+        <meshStandardMaterial color="white" />
+      </mesh>
     </>
   );
 };

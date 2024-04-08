33dba4d5979031a21d9f9196336fdb7f26f1322e
Chris Lane Jones <chrislanejones@gmail.com>
2024-04-08 02:48:10 -0400

    Camera Controls - 3D Animated Complex - Code Reorganized

--- courses/3-3-Camera-Controls/src/components/Experience.jsx
+++ courses/3-3-Camera-Controls/src/components/Experience.jsx
@@ -1,46 +1,31 @@
 import { button, useControls } from "leva";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { degToRad } from "three/src/math/MathUtils.js";
 import { sections } from "./UI";
 import { CameraControls, Environment, Gltf } from "@react-three/drei";
 
+const cameraPositions = {
+  intro: [0, 0, 3, 0, 0, 0],
+  titanium: [0, 0, 3, 0, 0, 0],
+  camera: [0, 0, 3, 0, 0, 0],
+  "action-button": [0, 0, 3, 0, 0, 0],
+};
+
 export const Experience = ({ section }) => {
-  const cameraPositions = {
-    intro: [0, 0, 3, 0, 0, 0],
-    titanium: [0, 0, 3, 0, 0, 0],
-    camera: [0, 0, 3, 0, 0, 0],
-    "action-button": [0, 0, 3, 0, 0, 0],
-  };
-
-  const [introFinished, setIntroFinished] = useState(false);
-  const intro = async () => {
-    controls.current.setLookAt(0, 0, 5, 0, 0, 0, false);
-    await controls.current.dolly(3, true);
-    await controls.current.rotate(degToRad(45), degToRad(25), true);
-
-    setIntroFinished(true);
-    playTransition();
-  };
-
-  useEffect(() => {
-    if (!introFinished) {
-      return;
-    }
-    playTransition();
-  }, [section]);
-  // ...
-
-  useControls("Helper", {
-    getLookAt: button(() => {
-      const position = controls.current.getPosition();
-      const target = controls.current.getTarget();
-      console.log([...position, ...target]);
-    }),
-  });
-
+  const controls = useRef();
   const box = useRef();
   const sphere = useRef();
 
+  /* Use Controls Section for leva */
+  useControls("settings", {
+    smoothTime: {
+      value: 0.35,
+      min: 0.1,
+      max: 2,
+      step: 0.1,
+      onChange: (v) => (controls.current.smoothTime = v),
+    },
+  });
   useControls("dolly", {
     in: button(() => {
       controls.current.dolly(1, true);
@@ -77,15 +62,6 @@ export const Experience = ({ section }) => {
       controls.current.rotate(0.5, 0, true);
     }),
   });
-  useControls("settings", {
-    smoothTime: {
-      value: 0.35,
-      min: 0.1,
-      max: 2,
-      step: 0.1,
-      onChange: (v) => (controls.current.smoothTime = v),
-    },
-  });
 
   useControls("fit", {
     fitToBox: button(() => {
@@ -95,7 +71,40 @@ export const Experience = ({ section }) => {
       controls.current.fitToSphere(sphere.current, false);
     }),
   });
-  const controls = useRef();
+
+  const [introFinished, setIntroFinished] = useState(false);
+  const intro = async () => {
+    controls.current.setLookAt(0, 0, 5, 0, 0, 0, false);
+    await controls.current.dolly(3, true);
+    await controls.current.rotate(degToRad(45), degToRad(25), true);
+
+    setIntroFinished(true);
+    playTransition();
+  };
+
+  const playTransition = () => {
+    controls.current.setLookAt(...cameraPositions[sections[section]], true);
+  };
+
+  useControls("Helper", {
+    getLookAt: button(() => {
+      const position = controls.current.getPosition();
+      const target = controls.current.getTarget();
+      console.log([...position, ...target]);
+    }),
+  });
+
+  useEffect(() => {
+    intro();
+  }, []);
+
+  useEffect(() => {
+    if (!introFinished) {
+      return;
+    }
+    playTransition();
+  }, [section]);
+
   return (
     <>
       <CameraControls ref={controls} />

996baf0915a0d680ccd90fb66d8abd6799466a52
Chris Lane Jones <chrislanejones@gmail.com>
2024-04-08 23:58:23 -0400

    Camera Controls - 3D Animated Complex - Responsive Added - Transitions Not Working

--- courses/3-3-Camera-Controls/src/components/Experience.jsx
+++ courses/3-3-Camera-Controls/src/components/Experience.jsx
@@ -5,19 +5,44 @@ import { sections } from "./UI";
 import { CameraControls, Environment, Gltf } from "@react-three/drei";
 
 const cameraPositions = {
-  intro: [0.8503144347555986, -0.560747178949297, 0.8503144347555988, 0, 0, 0],
+  intro: [
+    -0.8408130777453015, -0.5579311237409535, -1.2494838493879465,
+    0.09186000885066517, -0.013170057989714374, -0.15707536590700685,
+  ],
   titanium: [
-    1.6269470477745973, -0.08199617683753212, 0.0012868091538008235, 0, 0, 0,
+    -0.33845493114618996, -0.38300322828293054, 0.23117101681493882,
+    0.0907781098318133, 0.14091696753514726, -0.09426415209367653,
   ],
   camera: [
-    0.23787808955966433, -0.47834873508444364, 0.4750417529355832,
-    0.007050009547159786, 0.0028544709420128545, -0.017466759122731546,
+    -0.2816817625748494, -0.017347003209647244, 0.3065631638865963,
+    -0.036054404825409433, 0.2469066391985735, 0.01959945436374355,
   ],
   "action-button": [
-    0.5575016497944482, -0.028192851071981218, -0.00480673706298803,
-    -0.1855658916811315, 0.46422437600988475, -0.02354337756360715,
+    -0.88612937193474, -0.4964915367233699, 0.003459464080557469,
+    -0.015160554557400105, 0.16977404132378549, 0.0015889919991764756,
   ],
 };
+
+const cameraPositionsSmallScreen = {
+  intro: [
+    0.002795130059348545, -1.693376768600526, -1.4822074299185213,
+    -0.001428821463540626, 0.07667045240180789, 0.02673756366323502,
+  ],
+  titanium: [
+    -0.28281656325123505, -0.6454814943240393, 0.1753107005865352,
+    -0.03922954115167713, -0.09507212332370656, -0.05127510782733192,
+  ],
+  camera: [
+    -0.31909084746101646, -0.25505003995074715, 0.6646788535696884,
+    -0.07123702938574586, 0.23449640396985513, -0.0070772011186346715,
+  ],
+  "action-button": [
+    -0.6259770949105332, -0.2529443173252278, -0.004747347677011271,
+    -0.02485857424975495, 0.1912378489798071, -0.013710853350645076,
+  ],
+};
+
+const SMALL_SCREEN_THRESHOLD = 900;
 
 // const cameraPositions = {
 //   intro: [0, 0, 3, 0, 0, 0],
@@ -98,7 +123,14 @@ export const Experience = ({ section }) => {
   };
 
   const playTransition = () => {
-    controls.current.setLookAt(...cameraPositions[sections[section]], true);
+    if (window.innerWidth > SMALL_SCREEN_THRESHOLD) {
+      controls.current.setLookAt(...cameraPositions[sections[section]], true);
+    } else {
+      controls.current.setLookAt(
+        ...cameraPositionsSmallScreen[sections[section]],
+        true
+      );
+    }
   };
 
   useControls("Helper", {
@@ -122,7 +154,22 @@ export const Experience = ({ section }) => {
 
   return (
     <>
-      <CameraControls ref={controls} />
+      <CameraControls
+        ref={controls}
+        // disable all mouse buttons
+        mouseButtons={{
+          left: 0,
+          middle: 0,
+          right: 0,
+          wheel: 0,
+        }}
+        // disable all touch gestures
+        touches={{
+          one: 0,
+          two: 0,
+          three: 0,
+        }}
+      />
       <mesh ref={box} visible={false}>
         <boxGeometry args={[0.5, 0.8, 0.2]} />
         <meshBasicMaterial color="mediumpurple" wireframe />

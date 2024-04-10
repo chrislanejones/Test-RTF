180c0b66823dea5b620628ce6449f82606da7644
Chris Lane Jones <chrislanejones@gmail.com>
2024-04-10 01:50:21 -0400

    Render Target - Security Camera - useFBO - Rendering the Scene to a Texture

--- courses/3-5-Render-Target/src/components/Experience.jsx
+++ courses/3-5-Render-Target/src/components/Experience.jsx
@@ -14,6 +14,7 @@ import { useFrame } from "@react-three/fiber";
 const VECTOR_ZERO = new Vector3(0, 0, 0);
 
 export const Experience = () => {
+  const tvMaterial = useRef();
   const videoTexture = useVideoTexture("/textures/bounce-patrick.mp4");
   const cornerRenderTarget = useFBO();
 
@@ -22,6 +23,8 @@ export const Experience = () => {
     gl.setRenderTarget(cornerRenderTarget);
     gl.render(scene, camera);
     gl.setRenderTarget(null);
+
+    tvMaterial.current.map = cornerRenderTarget.texture;
   });
 
   return (
@@ -38,7 +41,7 @@ export const Experience = () => {
           <Gltf src="models/Room.glb" scale={0.3} rotation-y={-Math.PI / 2} />
           <mesh position-x={0.055} position-y={0.48} position-z={-0.601}>
             <planeGeometry args={[0.63, 0.44]} />
-            <meshBasicMaterial map={videoTexture} />
+            <meshBasicMaterial ref={tvMaterial} />
           </mesh>
         </group>
       </group>

37a30ff0a69a18541845bf933499b7467aa38b07
Chris Lane Jones <chrislanejones@gmail.com>
2024-04-13 23:48:07 -0400

    Post processing - Vignette - Leva

--- courses/3-6-Post-Processing/src/components/Effects.jsx
+++ courses/3-6-Post-Processing/src/components/Effects.jsx
@@ -1,6 +1,12 @@
 import { EffectComposer } from "@react-three/postprocessing";
+import { useControls } from "leva";
 
 export const Effects = () => {
+  const vignetteConfig = useControls("vignette", {
+    enabled: true,
+    offset: { value: 0.1, min: 0, max: 1 },
+    darkness: { value: 0.92, min: 0, max: 1 },
+  });
   return (
     <EffectComposer disableNormalPass>
       <Vignette />

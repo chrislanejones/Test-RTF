3b51d670a2a884dfa4de3a1ad715f3a73fd1b775
Chris Lane Jones <chrislanejones@gmail.com>
2024-04-13 23:57:42 -0400

    Post processing - Bloom -  luminanceThreshold, Intensity,  and mipmapBlur

--- courses/3-6-Post-Processing/src/components/Effects.jsx
+++ courses/3-6-Post-Processing/src/components/Effects.jsx
@@ -1,15 +1,25 @@
-import { EffectComposer, Vignette } from "@react-three/postprocessing";
+import { EffectComposer, Vignette, Bloom } from "@react-three/postprocessing";
 import { useControls } from "leva";
 
 export const Effects = () => {
-  const vignetteConfig = useControls("vignette", {
+  // Vignette Effect
+  // const vignetteConfig = useControls("vignette", {
+  //   enabled: true,
+  //   offset: { value: 0.1, min: 0, max: 1 },
+  //   darkness: { value: 0.92, min: 0, max: 1 },
+  // });
+
+  // Bloom Effect
+  const bloomConfig = useControls("bloom", {
     enabled: true,
-    offset: { value: 0.1, min: 0, max: 1 },
-    darkness: { value: 0.92, min: 0, max: 1 },
+    luminanceThreshold: { value: 1, min: 0, max: 2 },
+    intensity: { value: 1.28, min: 0, max: 2 },
+    mipmapBlur: true,
   });
+
   return (
     <EffectComposer disableNormalPass>
-      {vignetteConfig.enabled && <Vignette {...vignetteConfig} />}
+      {bloomConfig.enabled && <Bloom {...bloomConfig} />}
     </EffectComposer>
   );
 };

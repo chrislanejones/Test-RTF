e050b203a016127c8d60c61971166f6a355865a8
Chris Lane Jones <chrislanejones@gmail.com>
2024-05-16 23:58:33 -0400

    Theatre-JS  - AutoFocus

--- courses/3-7-Theatre-js/src/components/Experience.jsx
+++ courses/3-7-Theatre-js/src/components/Experience.jsx
@@ -1,6 +1,7 @@
 import { Environment } from "@react-three/drei";
 import { MedievalFantasyBook } from "./MedievalFantasyBook";
 import { editable as e } from "@theatre/r3f";
+import { Autofocus, EffectComposer } from "@react-three/postprocessing";
 
 export const Experience = () => {
   return (
@@ -18,6 +19,14 @@ export const Experience = () => {
         <MedievalFantasyBook scale={0.1} envMapIntensity={0.3} />
       </e.group>
       <Environment preset="dawn" background blur={4} />
+      <EffectComposer>
+        <Autofocus
+          smoothTime={0.1}
+          debug={0.04}
+          focusRange={0.002}
+          bokehScale={8}
+        />
+      </EffectComposer>
     </>
   );
 };

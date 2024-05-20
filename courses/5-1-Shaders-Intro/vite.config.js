38e00f11a6bc5a20c02a945cc26f240149374c27
Chris Lane Jones <chrislanejones@gmail.com>
2024-05-20 00:45:24 -0400

    Shaders-Intro - shaderMaterial - Added Red Square

--- courses/5-1-Shaders-Intro/vite.config.js
+++ courses/5-1-Shaders-Intro/vite.config.js
@@ -1,8 +1,7 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import glsl from "vite-plugin-glsl";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), glsl()],
-});
+  plugins: [react()],
+})

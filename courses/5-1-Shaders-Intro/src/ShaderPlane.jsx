81548e5a84dad92e980899b1aadb96cf1cf56e82
Chris Lane Jones <chrislanejones@gmail.com>
2024-05-20 02:21:49 -0400

    Shaders-Intro - shaderMaterial - Added Uniforms

--- courses/5-1-Shaders-Intro/src/ShaderPlane.jsx
+++ courses/5-1-Shaders-Intro/src/ShaderPlane.jsx
@@ -1,10 +1,17 @@
 import { shaderMaterial } from "@react-three/drei";
 import { extend } from "@react-three/fiber";
+import { Color } from "three";
 
 import myShaderFragment from "./shaders/myshader.fragment.glsl";
 import myShaderVertex from "./shaders/myshader.vertex.glsl";
 
-const MyShaderMaterial = shaderMaterial({}, myShaderVertex, myShaderFragment);
+const MyShaderMaterial = shaderMaterial(
+  {
+    uColor: new Color("pink"),
+  },
+  myShaderVertex,
+  myShaderFragment
+);
 
 extend({ MyShaderMaterial });
 
@@ -12,7 +19,7 @@ export const ShaderPlane = ({ ...props }) => {
   return (
     <mesh {...props}>
       <planeGeometry args={[1, 1]} />
-      <myShaderMaterial />
+      <myShaderMaterial uColor={"lightblue"} />
     </mesh>
   );
 };

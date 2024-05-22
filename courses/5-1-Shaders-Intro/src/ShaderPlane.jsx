37e4e10f17353f8b44f4e7d2730fe355b16e5478
Chris Lane Jones <chrislanejones@gmail.com>
2024-05-21 23:55:49 -0400

    Shaders-Intro - GLSL - Uniforms Clocks - uTime motion

--- courses/5-1-Shaders-Intro/src/ShaderPlane.jsx
+++ courses/5-1-Shaders-Intro/src/ShaderPlane.jsx
@@ -1,6 +1,7 @@
 import { shaderMaterial } from "@react-three/drei";
 import { extend, useFrame } from "@react-three/fiber";
 import { Color, Material } from "three";
+import { useRef } from "react";
 
 import myShaderFragment from "./shaders/myshader.fragment.glsl";
 import myShaderVertex from "./shaders/myshader.vertex.glsl";
--- courses/5-1-Shaders-Intro/src/shaders/myshader.vertex.glsl
+++ courses/5-1-Shaders-Intro/src/shaders/myshader.vertex.glsl
@@ -1,3 +1,11 @@
-  void main() {
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+uniform float uTime;
+
+void main() {
+  vec3 pos = position;
+  if (pos.x < 0.0) {
+    pos.y += sin(uTime) * 0.2;
+  } else {
+    pos.y += cos(uTime) * 0.2;
   }
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+}

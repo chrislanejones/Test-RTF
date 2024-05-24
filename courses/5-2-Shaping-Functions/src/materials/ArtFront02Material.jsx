b0d8a2a0824871bcfcb7525ce8f1712959bf9439
Chris Lane Jones <chrislanejones@gmail.com>
2024-05-23 23:24:06 -0400

    Shaping Functions - Practice Zone - Mix - White Two

--- courses/5-2-Shaping-Functions/src/materials/ArtFront02Material.jsx
+++ courses/5-2-Shaping-Functions/src/materials/ArtFront02Material.jsx
@@ -17,9 +17,16 @@ export const ArtFront02Material = shaderMaterial(
   uniform float uTime;
   varying vec2 vUv;
 
-  void main() {
+//   void main() {
+//   vec3 whiteColor = vec3(1.0);
+//   vec3 finalColor = mix(whiteColor, uColor, vUv.y);
+//   gl_FragColor = vec4(finalColor, 1.0);
+// }
+
+void main() {
   vec3 whiteColor = vec3(1.0);
-  vec3 finalColor = mix(whiteColor, uColor, vUv.y);
+  float pct = mix(0.0, 0.3, vUv.y);
+  vec3 finalColor = mix(whiteColor, uColor, pct);
   gl_FragColor = vec4(finalColor, 1.0);
 }
   `

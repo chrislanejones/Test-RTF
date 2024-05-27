a18408c11d4f18c0283f08306a0b8531c6dce68f
Chris Lane Jones <chrislanejones@gmail.com>
2024-05-26 20:11:47 -0400

    Shaping Functions -  Piet Mondrian - Added Green to the Canvas

--- courses/5-2-Shaping-Functions/src/materials/ArtFront02Material.jsx
+++ courses/5-2-Shaping-Functions/src/materials/ArtFront02Material.jsx
@@ -127,16 +127,46 @@ export const ArtFront02Material = shaderMaterial(
 // }
 
 // Combining effects (Broken Checkboard)
+// void main() {
+//     vec2 repeatedUvs = fract(vUv * 8.0 - 0.25);
+//     float verticalRectangle = step(0.5, repeatedUvs.x) * step(0.35, repeatedUvs.y);
+//     float horizontalLine = step(0.9, repeatedUvs.y);
+//     float pct = verticalRectangle + horizontalLine;
+//     vec3 finalColor = uColor * pct;
+//   gl_FragColor = vec4(finalColor, 1.0);
+// }
+
+// Piet Mondrian
+vec3 red    = vec3(0.81, 0.15, 0.15);
+vec3 blue   = vec3(0.215, 0.152, 0.615);
+vec3 yellow = vec3(0.89, 0.62, 0.26);
+vec3 yellowFluo = vec3(0.949,0.90,0.0627);
+vec3 white  = vec3(0.89, 0.89, 0.89);
+vec3 black  = vec3(0.22, 0.22, 0.22);
+vec3 green = vec3(0.28, 0.61,	0.29);
+
 void main() {
-    vec2 repeatedUvs = fract(vUv * 8.0 - 0.25);
-    float verticalRectangle = step(0.5, repeatedUvs.x) * step(0.35, repeatedUvs.y);
-    float horizontalLine = step(0.9, repeatedUvs.y);
-    float pct = verticalRectangle + horizontalLine;
-    vec3 finalColor = uColor * pct;
+  vec3 finalColor = white;
+
+  // Blue and Red Right
+  finalColor = mix(finalColor, black, step(0.5, vUv.x));
+  finalColor = mix(finalColor, red, step(0.55, vUv.x));
+  finalColor = mix(finalColor, black, step(0.5, vUv.x) * step(0.45, vUv.y));
+  finalColor = mix(finalColor, blue, step(0.55, vUv.x) * step(0.5, vUv.y));
+
+  finalColor = mix(finalColor, black, step(vUv.x, 0.5) * step(vUv.y, 0.24));
+  finalColor = mix(finalColor, yellow, step(vUv.x, 0.5) * step(vUv.y, 0.2));
+
+  finalColor = mix(finalColor, black, step(vUv.x, 0.5) * step(0.6, vUv.y) * step(vUv.y, 0.64));
+  finalColor = mix(finalColor, black, step(vUv.x, 0.25) * step(0.20, vUv.x) * step(0.64, vUv.y));
+
+  finalColor = mix(finalColor, green, step(vUv.x, 0.2) * step(0.2, vUv.y) * step(0.64, vUv.y));
+
+
+  finalColor = mix(finalColor, yellowFluo, step(vUv.x, 0.5) * step(0.25, vUv.x) * step(0.64, vUv.y) * step(vUv.y, 0.82));
+  finalColor = mix(finalColor, black, step(vUv.x, 0.5) * step(0.25, vUv.x) * step(0.82, vUv.y) * step(vUv.y, 0.86));
+
   gl_FragColor = vec4(finalColor, 1.0);
 }
-
-
-
  `
 );

71da56b8b964dd2c4a2c48842e624b172bc4ad5c
Chris Lane Jones <chrislanejones@gmail.com>
2024-05-29 23:46:42 -0400

    Shaping Functions - Animated shader - Reciprocal Animation

--- courses/5-2-Shaping-Functions/src/materials/ArtLeft02Material.jsx
+++ courses/5-2-Shaping-Functions/src/materials/ArtLeft02Material.jsx
@@ -100,18 +100,28 @@ float sdHexagram( in vec2 p, in float r )
 //     gl_FragColor = vec4(finalColor, 1.0);
 //   }
 
-  // Hexagram Mutipled With Reciprocal Animated with Color and SmoothStep
-  void main() {
+// Hexagram Mutipled With Reciprocal Animated with Color and SmoothStep
+//   void main() {
+//   vec2 translatedUvs = (vUv - 0.5) * 2.0;
+//   translatedUvs.x *= uResolution.x / uResolution.y;
+//   float hexagramDistance = sdHexagram(translatedUvs, 0.3);
+//   hexagramDistance = sin(hexagramDistance * 12.0 + uTime * 3.0) * 0.5 + 0.5;
+//   float pct = smoothstep(0.0, 0.8, hexagramDistance);
+//   vec3 finalColor = pct * uColor;
+//   gl_FragColor = vec4(finalColor, 1.0);
+// }
+
+// Hexagram Mutipled With Reciprocal Animation
+void main() {
   vec2 translatedUvs = (vUv - 0.5) * 2.0;
   translatedUvs.x *= uResolution.x / uResolution.y;
   float hexagramDistance = sdHexagram(translatedUvs, 0.3);
   hexagramDistance = sin(hexagramDistance * 12.0 + uTime * 3.0) * 0.5 + 0.5;
-  float pct = smoothstep(0.0, 0.8, hexagramDistance);
+  hexagramDistance /= 4.0;
+  float pct = 0.022 / hexagramDistance;
   vec3 finalColor = pct * uColor;
   gl_FragColor = vec4(finalColor, 1.0);
 }
-
-
 
   `
 );

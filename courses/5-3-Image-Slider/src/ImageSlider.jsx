43fec6d093f0b391300e775bc98c07102715c44f
Chris Lane Jones <chrislanejones@gmail.com>
2024-05-31 03:36:38 -0400

    Image Slider - Image Slider Component - Responsive Setup

--- courses/5-3-Image-Slider/src/ImageSlider.jsx
+++ courses/5-3-Image-Slider/src/ImageSlider.jsx
@@ -1,7 +1,14 @@
+import { useThree } from "@react-three/fiber";
+
 export const ImageSlider = ({ width = 3, height = 4, fillPercent = 0.75 }) => {
+  const viewport = useThree((state) => state.viewport);
+  let ratio = viewport.height / (height / fillPercent);
+  if (viewport.width < viewport.height) {
+    ratio = viewport.width / (width / fillPercent);
+  }
   return (
     <mesh>
-      <planeGeometry args={[width, height]} />
+      <planeGeometry args={[width * ratio, height * ratio]} />
       <meshBasicMaterial color="white" />
     </mesh>
   );

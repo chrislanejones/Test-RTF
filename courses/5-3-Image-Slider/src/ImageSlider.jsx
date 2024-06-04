7cceb6e3ec089206fd1771544232e7404cfa8a22
Chris Lane Jones <chrislanejones@gmail.com>
2024-06-03 23:56:55 -0400

    Image Slider - Image Transition Effect - Noise Effect

--- courses/5-3-Image-Slider/src/ImageSlider.jsx
+++ courses/5-3-Image-Slider/src/ImageSlider.jsx
@@ -26,16 +26,33 @@ const ImageSliderMaterial = shaderMaterial(
     uniform sampler2D uPrevTexture;
     uniform float uProgression;
     uniform int uDirection;
+
+    // Noise Added
+    float rand(vec2 n) {
+    return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
+    }
+
+    float noise(vec2 p){
+    vec2 ip = floor(p);
+    vec2 u = fract(p);
+    u = u*u*(3.0-2.0*u);
+
+    float res = mix(
+    mix(rand(ip),rand(ip+vec2(1.0,0.0)),u.x),
+    mix(rand(ip+vec2(0.0,1.0)),rand(ip+vec2(1.0,1.0)),u.x),u.y);
+    return res*res;
+    }
   
     void main() {
-    vec2 uv = vUv;
-    vec2 distortedPosition = vec2(uv.x - float(uDirection) * (1.0 - uProgression), uv.y);
-    vec4 curTexture = texture2D(uTexture, distortedPosition);
-              
-    vec2 distortedPositionPrev = vec2(uv.x + float(uDirection) * uProgression, uv.y);
-    vec4 prevTexture = texture2D(uPrevTexture, distortedPositionPrev);
-    
-    vec4 finalTexture = mix(prevTexture, curTexture, uProgression);
+      vec2 uv = vUv;
+      float noiseFactor = noise(gl_FragCoord.xy * 0.05);
+      vec2 distortedPosition = vec2(uv.x - float(uDirection) * (1.0 - uProgression) * noiseFactor, uv.y);
+      vec4 curTexture = texture2D(uTexture, distortedPosition);
+
+      vec2 distortedPositionPrev = vec2(uv.x + float(uDirection) * uProgression * noiseFactor, uv.y);
+      vec4 prevTexture = texture2D(uPrevTexture, distortedPositionPrev);
+      vec4 finalTexture = mix(prevTexture, curTexture, uProgression);
+
     gl_FragColor = finalTexture;
     #include <tonemapping_fragment>
     #include <encodings_fragment>

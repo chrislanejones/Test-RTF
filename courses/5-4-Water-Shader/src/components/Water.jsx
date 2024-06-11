1ae0a232340bb3228c6a0c084c22b35b1e402bde
Chris Lane Jones <chrislanejones@gmail.com>
2024-06-10 22:44:52 -0400

    Water Shader - Water Shader - Foam Effect - Partially Working

--- courses/5-4-Water-Shader/src/components/Water.jsx
+++ courses/5-4-Water-Shader/src/components/Water.jsx
@@ -1,19 +1,58 @@
+import { useFrame } from "@react-three/fiber";
 import { useControls } from "leva";
+import { useRef } from "react";
 import { Color } from "three";
 
 export const Water = ({ ...props }) => {
-  const { waterColor, waterOpacity } = useControls({
-    waterOpacity: { value: 0.8, min: 0, max: 1 },
-    waterColor: "#00c3ff",
+  const waterMaterialRef = useRef();
+  const { waterColor, waterOpacity, speed, noiseType, foam, foamTop, repeat } =
+    useControls({
+      waterOpacity: { value: 0.8, min: 0, max: 1 },
+      waterColor: "#00c3ff",
+      speed: { value: 0.5, min: 0, max: 5 },
+      repeat: {
+        value: 30,
+        min: 1,
+        max: 100,
+      },
+      foam: {
+        value: 0.4,
+        min: 0,
+        max: 1,
+      },
+      foamTop: {
+        value: 0.7,
+        min: 0,
+        max: 1,
+      },
+      noiseType: {
+        value: 0,
+        options: {
+          Perlin: 0,
+          Voronoi: 1,
+        },
+      },
+    });
+
+  useFrame(({ clock }) => {
+    if (waterMaterialRef.current) {
+      waterMaterialRef.current.uniforms.uTime.value = clock.getElapsedTime();
+    }
   });
 
   return (
     <mesh {...props}>
       <planeGeometry args={[15, 32, 22, 22]} />
-      <meshBasicMaterial
-        color={waterColor}
+      <waterMaterial
+        ref={waterMaterialRef}
+        uColor={new Color(waterColor)}
         transparent
-        opacity={waterOpacity}
+        uOpacity={waterOpacity}
+        uNoiseType={noiseType}
+        uSpeed={speed}
+        uRepeat={repeat}
+        uFoam={foam}
+        uFoamTop={foamTop}
       />
     </mesh>
   );
--- courses/5-4-Water-Shader/src/components/WaterMaterial.jsx
+++ courses/5-4-Water-Shader/src/components/WaterMaterial.jsx
@@ -6,6 +6,12 @@ export const WaterMaterial = shaderMaterial(
   {
     uColor: new Color("skyblue"),
     uOpacity: 0.8,
+    uTime: 0,
+    uSpeed: 0.5,
+    uRepeat: 20.0,
+    uNoiseType: 0,
+    uFoam: 0.4,
+    uFoamTop: 0.7,
   },
   /*glsl*/ `
   varying vec2 vUv;
@@ -14,17 +20,24 @@ export const WaterMaterial = shaderMaterial(
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
   }`,
   resolveLygia(/*glsl*/ `
-      #include "lygia/generative/pnoise.glsl"
-      varying vec2 vUv;
-      uniform vec3 uColor;
-      uniform float uOpacity;
+    #include "lygia/generative/pnoise.glsl"
+    varying vec2 vUv;
+    uniform vec3 uColor;
+    uniform float uOpacity;
+    uniform float uTime;
+    uniform float uSpeed;
+    uniform float uRepeat;
+    uniform int uNoiseType;
+    uniform float uFoam;
+    uniform float uFoamTop;
     
-    void main() {
-      float noise = pnoise(vec3(vUv * 10.0, 1.0), vec3(100.0, 24.0, 112.0));
-      vec3 black = vec3(0.0);
-      vec3 finalColor = mix(uColor, black, noise);
-      gl_FragColor = vec4(finalColor, uOpacity);
-      #include <tonemapping_fragment>
-      #include <encodings_fragment>
-    }`)
+      void main() {
+  float noise = pnoise(vec3(vUv * 10.0, 1.0), vec3(100.0, 24.0, 112.0));
+  vec3 black = vec3(0.0);
+  vec3 finalColor = mix(uColor, black, noise);
+  gl_FragColor = vec4(finalColor, uOpacity);
+  #include <tonemapping_fragment>
+  #include <encodings_fragment>
+}
+`)
 );

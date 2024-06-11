c55c205f2eef643c4a60c44cf00d07aa67d09ae7
Chris Lane Jones <chrislanejones@gmail.com>
2024-06-10 21:49:39 -0400

    Water Shader - Water Shader - Resolve Lygia - pnoise

--- courses/5-4-Water-Shader/src/components/WaterMaterial.jsx
+++ courses/5-4-Water-Shader/src/components/WaterMaterial.jsx
@@ -1,7 +1,6 @@
-import { resolveLygia } from "resolve-lygia";
-
 import { shaderMaterial } from "@react-three/drei";
 import { Color } from "three";
+import { resolveLygia } from "resolve-lygia";
 
 export const WaterMaterial = shaderMaterial(
   {
@@ -9,18 +8,20 @@ export const WaterMaterial = shaderMaterial(
     uOpacity: 0.8,
   },
   /*glsl*/ `
-  #include "lygia/generative/pnoise.glsl"
   varying vec2 vUv;
   void main() {
     vUv = uv;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
   }`,
-  resolveLygia(/*glsl*/ ` 
+  resolveLygia(/*glsl*/ `
+    #include "lygia/generative/pnoise.glsl" 
     varying vec2 vUv;
     uniform vec3 uColor;
     uniform float uOpacity;
 
     void main() {
+      // pnoise - https://lygia.xyz/generative/pnoise
+      float noise = pnoise(vec3(vUv * 10.0, 1.0), vec3(100.0, 24.0, 112.0));
       gl_FragColor = vec4(uColor, uOpacity);
       #include <tonemapping_fragment>
       #include <encodings_fragment>

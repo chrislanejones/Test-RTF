ddb897bc75361990b9b9672b3e02720e0d5c4471
Chris Lane Jones <chrislanejones@gmail.com>
2024-07-07 23:26:31 -0400

    Shader Trans - Screen Transition - Fullscreen Plane

--- 5-5-Shader-Transitions/src/components/ScreenTransition.jsx
+++ 5-5-Shader-Transitions/src/components/ScreenTransition.jsx
@@ -1,8 +1,21 @@
+import { Hud, OrthographicCamera } from "@react-three/drei";
+
 export const ScreenTransition = ({ transition, color }) => {
   return (
-    <mesh>
-      <planeGeometry args={[2, 2]} />
-      <meshBasicMaterial color={color} />
-    </mesh>
+    <Hud>
+      <OrthographicCamera
+        makeDefault
+        top={1}
+        right={1}
+        bottom={-1}
+        left={-1}
+        near={0}
+        far={1}
+      />
+      <mesh>
+        <planeGeometry args={[2, 2]} />
+        <meshBasicMaterial color={color} />
+      </mesh>
+    </Hud>
   );
 };

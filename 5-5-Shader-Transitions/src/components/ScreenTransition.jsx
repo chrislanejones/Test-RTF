ce7ba9e3f141623286db9d56ffbd416f9636aaa9
Chris Lane Jones <chrislanejones@gmail.com>
2024-07-09 23:58:25 -0400

    Shader Trans - Screen Transition - TransitionData useRef

--- 5-5-Shader-Transitions/src/components/ScreenTransition.jsx
+++ 5-5-Shader-Transitions/src/components/ScreenTransition.jsx
@@ -1,7 +1,39 @@
 import { Hud, OrthographicCamera, shaderMaterial } from "@react-three/drei";
 import { extend } from "@react-three/fiber";
-import { useRef } from "react";
 import { Color } from "three";
+import { useFrame } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
+import { MathUtils } from "three/src/math/MathUtils.js";
+import { TRANSITION_DURATION } from "./UI";
+
+export const ScreenTransition = ({ transition, color }) => {
+  const transitionMaterial = useRef();
+  const transitionData = useRef({
+    from: 0,
+    to: 0,
+    started: 0,
+  });
+
+  useEffect(() => {
+    transitionData.current.from =
+      transition && transitionData.current.started ? 1 : 0;
+    transitionData.current.to = transition ? 0 : 1;
+    transitionData.current.started = new Date();
+  }, [transition]);
+
+  useFrame(() => {
+    if (!transitionMaterial.current) {
+      return;
+    }
+    transitionMaterial.current.uniforms.uProgression.value = MathUtils.lerp(
+      transitionData.current.from,
+      transitionData.current.to,
+      (new Date() - transitionData.current.started) /
+        (TRANSITION_DURATION * 1000)
+    );
+  );
+});
+
 
 export const ScreenTransition = ({ transition, color }) => {
   const transitionMaterial = useRef();

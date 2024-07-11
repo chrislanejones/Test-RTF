7f4f4a29995a829e9eb87eca92be1fc45204883f
Chris Lane Jones <chrislanejones@gmail.com>
2024-07-10 23:58:47 -0400

    Shader Trans - Screen Transition - Fixing Error

--- 5-5-Shader-Transitions/src/components/ScreenTransition.jsx
+++ 5-5-Shader-Transitions/src/components/ScreenTransition.jsx
@@ -2,6 +2,9 @@ import { Hud, OrthographicCamera, shaderMaterial } from "@react-three/drei";
 import { extend } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
 import { Color } from "three";
+import { useFrame } from "@react-three/fiber";
+import { MathUtils } from "three/src/math/MathUtils.js";
+import { TRANSITION_DURATION } from "./UI";
 
 const ScreenTransitionMaterial = shaderMaterial(
   {
@@ -15,13 +18,7 @@ const ScreenTransitionMaterial = shaderMaterial(
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
   }`,
   /* glsl */ `
-  uniform vec3 uColor;
-  varying vec2 vUv;
-  uniform float uProgression;
-  const float pi = 3.141592654;
-
-  void main() {
-    vec2 uvs = vUv - 0.5;
+  vec2 uvs = vUv - 0.5;
   float r = length(uvs * 0.92);
   float theta = atan(uvs.y, uvs.x);
   float spiral = fract(2.5 * theta / pi + 7.0 * pow(r, 0.4) - 4.5 * uProgression);
@@ -30,8 +27,8 @@ const ScreenTransitionMaterial = shaderMaterial(
   alpha = min(alpha, spiral);
   vec3 finalColor = uColor;
 
-    gl_fragment = vec4(finalColor, alpha);
-    #include <encodings_fragment>
+  gl_FragColor = vec4(finalColor, alpha);
+  #include <encodings_fragment>
   }`
 );
 
@@ -48,7 +45,21 @@ export const ScreenTransition = ({ transition, color }) => {
   useEffect(() => {
     transitionData.current.from =
       transition && transitionData.current.started ? 1 : 0;
+    transitionData.current.to = transition ? 0 : 1;
+    transitionData.current.started = new Date();
   }, [transition]);
+
+  useFrame(() => {
+    if (!transitionMaterial.current) {
+      return;
+    }
+    transitionMaterial.current.uniforms.uProgression.value = MathUtils.lerp(
+      transitionData.current.from,
+      transitionData.current.to,
+      (new Date() - transitionData.current.started) /
+        (TRANSITION_DURATION * 1000)
+    );
+  });
   return (
     <Hud>
       <OrthographicCamera

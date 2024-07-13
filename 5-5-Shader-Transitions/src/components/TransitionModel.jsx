8ab382ae3d7e77ea9d7c0ba895b4fe142c16a319
Chris Lane Jones <chrislanejones@gmail.com>
2024-07-12 23:56:20 -0400

    Shader Trans - Model Transition - Setup Transition Logic

--- 5-5-Shader-Transitions/src/components/TransitionModel.jsx
+++ 5-5-Shader-Transitions/src/components/TransitionModel.jsx
@@ -1,8 +1,28 @@
 import { useGLTF } from "@react-three/drei";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { MathUtils } from "three/src/math/MathUtils.js";
+import { CAKE_TRANSITION_DURATION } from "./UI";
 
 export function TransitionModel({ model, visible, ...props }) {
   const { scene } = useGLTF(`/models/${model}.glb`);
+
+  const transitionData = useRef({
+    from: 0,
+    to: 1,
+    started: 0,
+  });
+
+  const [animatedVisible, setAnimatedVisible] = useState(visible);
+
+  useEffect(() => {
+    if (visible == animatedVisible) {
+      return;
+    }
+    if (!visible) {
+      transitionData.current.from = 1;
+    }
+    const timeout = setTimeout(() => {}, CAKE_TRANSITION_DURATION * 1000);
+  }, [visible]);
 
   return (
     <group {...props} dispose={null} visible={visible}>

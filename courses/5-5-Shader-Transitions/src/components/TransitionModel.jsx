b7e6d39fda7b63bcf8e22fd54e32c62ebfface23
Chris Lane Jones <chrislanejones@gmail.com>
2024-07-14 23:58:14 -0400

    Shader Trans - Model Transition - Dissolve Effect

--- courses/5-5-Shader-Transitions/src/components/TransitionModel.jsx
+++ courses/5-5-Shader-Transitions/src/components/TransitionModel.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { MathUtils } from "three/src/math/MathUtils.js";
 import { CAKE_TRANSITION_DURATION } from "./UI";
 import { useFrame } from "@react-three/fiber";
+import { shaderStages } from "three/examples/jsm/nodes/Nodes.js";
 
 const declarationsFragment = /* glsl */ `
   uniform float uProgression;
@@ -12,29 +13,17 @@ const fadeFragment = /* glsl */ `
   diffuseColor.a = diffuseColor.a * uProgression;
 `;
 
+const varyingFragment = /* glsl */ `
+  varying vec3 vPosition;
+`;
+
+const applyVaryingFragment = /* glsl */ `
+  // use world position to apply the effect
+  vPosition = gl_Position.xyz;
+`;
+
 export function TransitionModel({ model, visible, ...props }) {
   const { scene, materials } = useGLTF(`/models/${model}.glb`);
-  console.log(materials);
-
-  useEffect(() => {
-    Object.values(materials).forEach((material) => {
-      material.transparent = true;
-      material.onBeforeCompile = (shader) => {
-        shader.uniforms.uProgression = { value: 0 };
-        material.userData.shader = shader;
-        shader.fragmentShader = shader.fragmentShader.replace(
-          `void main() {`,
-          `${declarationsFragment}
-            void main() {`
-        );
-        shader.fragmentShader = shader.fragmentShader.replace(
-          `#include <alphamap_fragment>`,
-          `#include <alphamap_fragment>
-          ${fadeFragment}`
-        );
-      };
-    });
-  }, [materials]);
   const transitionData = useRef({
     from: 0,
     to: 1,
@@ -75,6 +64,34 @@ export function TransitionModel({ model, visible, ...props }) {
       }
     });
   });
+
+  useEffect(() => {
+    Object.values(materials).forEach((material) => {
+      material.transparent = true;
+      material.onBeforeCompile = (shader) => {
+        shader.fragmentShader = shader.fragmentShader.replace(
+          shader.vertexShader = shader.vertexShader.replace(
+            `void main() {`,
+            `${varyingFragment}
+                    void main() {`
+          );
+          shader.vertexShader = shader.vertexShader.replace(
+            `#include <fog_vertex>`,
+            `#include <fog_vertex>
+                    ${applyVaryingFragment}`
+          );
+          `void main() {`,
+          `${declarationsFragment}
+          void main() {`
+        );
+        shader.fragmentShader = shader.fragmentShader.replace(
+          `#include <alphamap_fragment>`,
+          `#include <alphamap_fragment>
+        ${fadeFragment}`
+        );
+      };
+    });
+  }, [materials]);
 
   return (
     <group {...props} dispose={null} visible={animatedVisible}>
